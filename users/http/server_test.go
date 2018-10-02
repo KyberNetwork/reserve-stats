dfd39000e6a0c346d3ae554b87ac0a576f2dead3
Hoàng Hà <halink0803@gmail.com>
2018-10-02 15:46:08 +0700

    update tests

--- users/http/server_test.go
+++ users/http/server_test.go
@@ -3,21 +3,29 @@ package http
 import (
 	"fmt"
 	"net/http"
-	"os"
 	"testing"
+	"time"
 
 	"github.com/KyberNetwork/reserve-stats/users/cmc"
+	"github.com/KyberNetwork/reserve-stats/users/http/httputil"
 	"github.com/KyberNetwork/reserve-stats/users/stats"
 	"github.com/KyberNetwork/reserve-stats/users/storage"
 	"go.uber.org/zap"
 )
 
+const (
+	postgresHost     = "127.0.0.1:5432"
+	postgresUser     = "reserve_stats"
+	postgresPassword = "reserve_stats"
+	postgresDatabase = "reserve_stats"
+)
+
 func connectToTestDB() *storage.UserDB {
 	return storage.NewDB(
-		os.Getenv("POSTGRES_HOST"),
-		os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PASSWORD"),
-		os.Getenv("POSTGRESS_DATABASE"),
+		postgresHost,
+		postgresUser,
+		postgresPassword,
+		postgresDatabase,
 	)
 }
 
@@ -28,6 +36,9 @@ func clearTestDB() {
 func TestUserHTTPServer(t *testing.T) {
 	userStorage := connectToTestDB()
 	cmc := cmc.NewCMCEthUSDRate()
+	// sleep so cmc fetcher can get rate from cmc
+	time.Sleep(1 * time.Second)
+
 	userStats := stats.NewUserStats(cmc, userStorage)
 	defer clearTestDB()
 
@@ -35,6 +46,7 @@ func TestUserHTTPServer(t *testing.T) {
 	//TODO: turn into variable
 	host := fmt.Sprintf(":%s", "9000")
 	s := NewServer(userStats, host)
+	s.register()
 
 	zap.S().Infof("Server instance: %+v", s)
 
@@ -44,19 +56,74 @@ func TestUserHTTPServer(t *testing.T) {
 		userEmail       = "test@gmail.com"
 		wrongUserEmail  = "test"
 
-		userAddresses          = "0xc9a658f87d7432ff897f31dce318f0856f66acb7_0x2ea6200a999f4c6c982be525f8dc294f14f4cb08"
+		userAddresses          = "0xc9a658f87d7432ff897f31dce318f0856f66acb7-0x2ea6200a999f4c6c982be525f8dc294f14f4cb08"
 		wrongUserAddresses     = "wrong-address_0x13197"
-		wrongNumberOfAddresses = "0xc9a658f87d7432ff897f31dce318f0856f66acb7_0x2ea6200a999f4c6c982be525f8dc294f14f4cb08_0x4e012a6445ba2a590b8b1ee4e95d03e345a0c2e5"
-		userTimeStamp          = "1538380670000_1538380682000"
+		wrongNumberOfAddresses = "0xc9a658f87d7432ff897f31dce318f0856f66acb7-0x2ea6200a999f4c6c982be525f8dc294f14f4cb08-0x4e012a6445ba2a590b8b1ee4e95d03e345a0c2e5"
+		userTimeStamp          = "1538380670000-1538380682000"
+		queryAddress           = "0xc9a658f87d7432ff897f31dce318f0856f66acb7"
+		nonKycAddress = "0xb8df4cf4b7ad086cd5139a75033566164e41a0b4"
 	)
 
 	var tests = []testCase{
 		{
-			msg:      "test get empty db",
-			endpoint: requestEndpoint,
+			msg:      "empty db",
+			endpoint: fmt.Sprintf("%s/%s", requestEndpoint, queryAddress),
 			method:   http.MethodGet,
-			assertFn: httputil.ExpectSuccess,
+			assert:   httputil.ExpectSuccess,
+		},
+		{
+			msg:      "update user addresses with wrong number of addresses",
+			endpoint: requestEndpoint,
+			method:   http.MethodPost,
+			data: map[string]string{
+				"user":       userEmail,
+				"addresses":  wrongNumberOfAddresses,
+				"timestamps": userTimeStamp,
+			},
+			assert: httputil.ExpectFailure,
+		},
+		// { msg:      "wrong user addresses",
+		// 	endpoint: requestEndpoint,
+		// 	method:   http.MethodPost,
+		// 	data: map[string]string{
+		// 		"user":       userEmail,
+		// 		"addresses":  wrongUserAddresses,
+		// 		"timestamps": userTimeStamp,
+		// 	},
+		// 	assert: httputil.ExpectFailure,
+		// },
+		{
+			msg: "request malformed",
+			endpoint: requestEndpoint,
+			method: http.MethodPost,
+			assert: httputil.ExpectFailure,
+		},
+		{
+			msg: "update correct user addresses",
+			endpoint: requestEndpoint,
+			method: http.MethodPost,
+			data: map[string]string{
+				"user": userEmail,
+				"addresses": userAddresses,
+				"timestamps": userTimeStamp,
+			},
+			assert: httputil.ExpectSuccess,
+		},
+		{
+			msg: "user is not kyced",
+			endpoint: fmt.Sprintf("%s/%s", requestEndpoint, nonKycAddress),
+			method: http.MethodGet,
+			assert: httputil.ExpectNonKYC,
+		},
+		{
+			msg: "user is kyced",
+			endpoint: fmt.Sprintf("%s/%s", requestEndpoint, queryAddress),
+			method: http.MethodGet,
+			assert: httputil.ExpectKYC,
 		},
 	}
 
+	for _, tc := range tests {
+		t.Run(tc.msg, func(t *testing.T) { testHTTPRequest(t, tc, s.r) })
+	}
 }

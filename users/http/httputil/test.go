e94f924266f704b87b12618ef3aa01a0bb16d326
Hoàng Hà <halink0803@gmail.com>
2018-10-02 15:46:08 +0700

    update tests

--- users/http/httputil/test.go
+++ users/http/httputil/test.go
@@ -11,6 +11,11 @@ type responseBody struct {
 	Success bool
 }
 
+type userResponse struct {
+	Success bool `json:"success"`
+	KYC     bool `json:"kyced"`
+}
+
 // expectStatus asserts that given response matches the expected status.
 func expectStatus(t *testing.T, resp *httptest.ResponseRecorder, success bool) {
 	t.Helper()
@@ -39,4 +44,33 @@ func ExpectFailure(t *testing.T, resp *httptest.ResponseRecorder) {
 	t.Helper()
 
 	expectStatus(t, resp, false)
+}
+
+func expectKYC(t *testing.T, resp *httptest.ResponseRecorder, kyc bool) {
+	t.Helper()
+
+	if resp.Code != http.StatusOK {
+		t.Fatalf("wrong return code, expected: %d, got: %d", http.StatusOK, resp.Code)
+	}
+	decoded := &userResponse{}
+	if err := json.NewDecoder(resp.Body).Decode(decoded); err != nil {
+		t.Fatal(err)
+	}
+	if decoded.KYC != kyc {
+		t.Errorf("wrong kyc status, expected: %t, got: %t", kyc, decoded.KYC)
+	}
+}
+
+//ExpectKYC assert that givien response is a kyced user
+func ExpectKYC(t *testing.T, resp *httptest.ResponseRecorder) {
+	t.Helper()
+
+	expectKYC(t, resp, true)
+}
+
+//ExpectNonKYC assert that given response is a non kyced user
+func ExpectNonKYC(t *testing.T, resp *httptest.ResponseRecorder) {
+	t.Helper()
+
+	expectKYC(t, resp, false)
 }

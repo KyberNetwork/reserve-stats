7ab4996cbea9c421069d2cb8e08a5310cddad78f
ductm54 <ductm54@gmail.com>
2018-10-02 16:19:18 +0700

    add test

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ require (
 	github.com/aristanetworks/goarista v0.0.0-20180907105523-ff33da284e76 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20180720115003-f9ffefc3facf // indirect
 	github.com/btcsuite/btcd v0.0.0-20180924021209-2a560b2036be // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/deckarep/golang-set v1.7.1 // indirect
 	github.com/ethereum/go-ethereum v1.8.16
 	github.com/gin-contrib/sse v0.0.0-20170109093832-22d885f9ecc7 // indirect
@@ -19,8 +20,11 @@ require (
 	github.com/oschwald/geoip2-golang v1.2.1
 	github.com/oschwald/maxminddb-golang v1.3.0 // indirect
 	github.com/pborman/uuid v1.2.0 // indirect
+	github.com/pkg/errors v0.8.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rjeczalik/notify v0.9.2 // indirect
 	github.com/rs/cors v1.5.0 // indirect
+	github.com/stretchr/testify v1.2.2 // indirect
 	github.com/syndtr/goleveldb v0.0.0-20180815032940-ae2bd5eed72d // indirect
 	github.com/ugorji/go/codec v0.0.0-20180927125128-99ea80c8b19a // indirect
 	github.com/urfave/cli v1.20.0
---
+++ ipinfo/ip_locator_test.go
@@ -0,0 +1,60 @@
+package ipinfo
+
+import (
+	"fmt"
+	"testing"
+
+	"go.uber.org/zap"
+)
+
+func createIPLocator() (*Locator, error) {
+	logger, _ := zap.NewDevelopment()
+	defer logger.Sync()
+	sugar := logger.Sugar()
+	return NewLocator(sugar, ".")
+}
+
+//TestValidIP test when input is a valid IP of US
+func TestValidIP(t *testing.T) {
+	l, err := createIPLocator()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	ip := "8.8.8.8"
+	country, err := l.IPToCountry(ip)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	if country != "US" {
+		t.Errorf("Get location of ip %s was incorrect, got %s, want %s", ip, country, "US")
+	}
+}
+
+//TestValidIP test when input is an invalid IP
+func TestInvalidIP(t *testing.T) {
+	l, err := createIPLocator()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	ip := "22"
+	_, err = l.IPToCountry(ip)
+	if err.Error() != fmt.Sprintf("%s is invalid ip", ip) {
+		t.Errorf(err.Error())
+	}
+}
+
+//TestNotFoundIP test when input ip could not be locate
+func TestNotFoundIP(t *testing.T) {
+	l, err := createIPLocator()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	ip := "192.168.0.1"
+	country, err := l.IPToCountry(ip)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	if country != "" {
+		t.Errorf("Get location of ip %s was incorrect, got %s, want %s", ip, country, "US")
+	}
+}

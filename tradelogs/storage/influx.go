9f8b9ed22962b909cb507a3c4920e4d78bd6687c
Nam Nguyen <namnm1991@gmail.com>
2018-10-08 16:18:15 +0700

    Update trade point
    - Use precision milliseconds 'ms'
    - Use block_number, tx_hash, eth_receival_sender, ip as tag of
    trade to query later.

--- tradelogs/storage/influx.go
+++ tradelogs/storage/influx.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"fmt"
 	"github.com/influxdata/influxdb/client/v2"
+	"strconv"
 
 	"github.com/KyberNetwork/reserve-stats/lib/blockchain"
 	"github.com/KyberNetwork/reserve-stats/tradelogs/common"
@@ -29,7 +30,7 @@ func NewInfluxStorage(dbName string, c client.Client, tokenUtil *blockchain.Toke
 func (is *InfluxStorage) SaveTradeLogs(logs []common.TradeLog) error {
 	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
 		Database:  is.dbName,
-		Precision: "s",
+		Precision: "ms",
 	})
 	if err != nil {
 		return err
@@ -79,7 +80,10 @@ func (is *InfluxStorage) queryDB(clnt client.Client, cmd string) (res []client.R
 
 func (is *InfluxStorage) tradeLogToPoint(log common.TradeLog) (*client.Point, error) {
 	tags := map[string]string{
-		"eth_sender": log.EtherReceivalSender.String(),
+		"block_number": strconv.FormatUint(log.BlockNumber, 10),
+		"tx_hash":      log.TransactionHash.String(),
+
+		"eth_receival_sender": log.EtherReceivalSender.String(),
 
 		"user_addr": log.UserAddress.String(),
 
@@ -90,9 +94,10 @@ func (is *InfluxStorage) tradeLogToPoint(log common.TradeLog) (*client.Point, er
 		"wallet_addr":  log.WalletAddress.String(),
 
 		"country": log.Country,
+		"ip":      log.IP,
 	}
 
-	ethAmount, err := is.tokenUtil.GetTokenAmount(blockchain.ETHAddr, log.EtherReceivalAmount)
+	ethReceivalAmount, err := is.tokenUtil.GetTokenAmount(blockchain.ETHAddr, log.EtherReceivalAmount)
 	if err != nil {
 		return nil, err
 	}
@@ -118,11 +123,7 @@ func (is *InfluxStorage) tradeLogToPoint(log common.TradeLog) (*client.Point, er
 	}
 
 	fields := map[string]interface{}{
-		"block_number": int64(log.BlockNumber),
-		"tx_hash":      log.TransactionHash.String(),
-		"tx_index":     int64(log.Index),
-
-		"eth_amount": ethAmount,
+		"eth_receival_amount": ethReceivalAmount,
 
 		"src_amount":  srcAmount,
 		"dst_amount":  dstAmount,
@@ -130,11 +131,9 @@ func (is *InfluxStorage) tradeLogToPoint(log common.TradeLog) (*client.Point, er
 
 		"wallet_fee": walletFee,
 		"burn_fee":   burnFee,
-
-		"ip": log.IP,
 	}
 
-	pt, err := client.NewPoint("trade_logs", tags, fields, log.Timestamp)
+	pt, err := client.NewPoint("trade", tags, fields, log.Timestamp)
 	if err != nil {
 		return nil, err
 	}

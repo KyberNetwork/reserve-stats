5d03de86586bed8cc0d6f25461a5f352c3d28283
kyberliem <cyberliem.civil@gmail.com>
2018-10-09 10:03:25 +0700

    implemented influx client and reserve_rate schema

---
+++ reserve-rates-crawler/storage/influx/influxSchema.go
@@ -0,0 +1,29 @@
+package influx
+
+// RateSchemaFieldName define a list of field names for a rate record in influxDB
+//go:generate stringer -type=RateSchemaFieldName -linecomment
+type RateSchemaFieldName int
+
+const (
+	Time           RateSchemaFieldName = iota //time
+	ReturnTime                                //return_time
+	Pair                                      //pair
+	BuyRate                                   //buy_rate
+	SellRate                                  //sell_rate
+	BuySanityRate                             //buy_sanity_rate
+	SellSanityRate                            //sell_sanity_rate
+	BlockNumber                               //block_number
+	Reserve                                   //reserve
+)
+
+var RateSchemaFields = map[string]RateSchemaFieldName{
+	"time":             Time,
+	"return_time":      ReturnTime,
+	"pair":             Pair,
+	"buy_rate":         BuyRate,
+	"sell_rate":        SellRate,
+	"buy_sanity_rate":  BuySanityRate,
+	"sell_sanity_rate": SellSanityRate,
+	"block_number":     BlockNumber,
+	"reserve":          Reserve,
+}
--- reserve-rates-crawler/storage/rate_storage.go
+++ reserve-rates-crawler/storage/rate_storage.go
@@ -2,9 +2,11 @@ package storage
 
 import (
 	"github.com/KyberNetwork/reserve-stats/reserve-rates-crawler/common"
+	ethereum "github.com/ethereum/go-ethereum/common"
 )
 
 //ReserveRatesStorage defines a set of interface for reserve rate storage, which can be implemented by any DB
 type ReserveRatesStorage interface {
 	UpdateRatesRecords(rateRecords map[string]common.ReserveRates) error
+	GetRatesByTimePoint(rsvAddr ethereum.Address, fromTime, toTime int64) ([]common.ReserveRates, error)
 }

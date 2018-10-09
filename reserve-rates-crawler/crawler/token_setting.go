2ada6be23ab57573a23f8f318946e74639098585
kyberliem <cyberliem.civil@gmail.com>
2018-10-09 18:28:45 +0700

    change crawler's components into interfaces

--- reserve-rates-crawler/crawler/token_setting.go
+++ reserve-rates-crawler/crawler/token_setting.go
@@ -2,8 +2,8 @@ package crawler
 
 import "github.com/KyberNetwork/reserve-stats/lib/core"
 
-// TokenSetting defines a set of interface to query for the current reserve setting.
-type TokenSetting interface {
+// tokenSetting defines a set of interface to query for the current reserve setting.
+type tokenSetting interface {
 	// GetInternalTokens returns a set of internal token from reserve configuration or error if occurs
 	GetInternalTokens() ([]core.Token, error)
 	// GetActiveToken returns a set of active token from reserve configuration or error if occurs

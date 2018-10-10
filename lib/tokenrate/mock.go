5a68c47b1730f41cd546c164641aa552afc2159e
Diep Pham <me@favadi.com>
2018-10-10 13:21:14 +0700

    add missing docs

--- lib/tokenrate/mock.go
+++ lib/tokenrate/mock.go
@@ -10,10 +10,12 @@ func NewMock() *Mock {
 // Mock is a mock tokenrate.Provider to use in tests.
 type Mock struct{}
 
+// Rate is a mock method to satisfy the tokenrate.Provider interface.
 func (m *Mock) Rate(_, _ string, _ time.Time) (float64, error) {
 	return 100, nil
 }
 
+// USDRate is a mock method to satisfy the tokenrate.USDRateProvider interface.
 func (m *Mock) USDRate(_ time.Time) (float64, error) {
 	return m.Rate("", "", time.Time{})
 }

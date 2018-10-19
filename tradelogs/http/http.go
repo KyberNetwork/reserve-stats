691a4a444f50241ec2bb44dfee4d86d0e3f99ea7
Nam Nguyen <namnm1991@gmail.com>
2018-10-19 11:48:47 +0700

    Validate parameter time for trade logs endpoint
    - Limit time range to 1 day
    - Set default value if param is missing

--- tradelogs/http/http.go
+++ tradelogs/http/http.go
@@ -3,13 +3,14 @@ package http
 import (
 	"fmt"
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
 
 	"github.com/KyberNetwork/reserve-stats/tradelogs/storage"
 )
+
+const limitedTimeRange = 24 * time.Hour
 
 // Server serve trade logs through http endpoint
 type Server struct {
@@ -18,45 +19,58 @@ type Server struct {
 	addr    string
 }
 
+type tradeLogsQuery struct {
+	From uint64 `form:"from"`
+	To   uint64 `form:"to"`
+}
+
 func (ha *Server) getTradeLogs(c *gin.Context) {
-	from, err := strconv.ParseInt(c.Query("from"), 10, 64)
-	if err != nil {
+	var query tradeLogsQuery
+	if err := c.ShouldBindQuery(&query); err != nil {
 		c.JSON(
 			http.StatusBadRequest,
-			gin.H{"success": false, "reason": fmt.Errorf("invalid from time %s", err.Error())},
-		)
-		return
-	}
-	to, err := strconv.ParseInt(c.Query("to"), 10, 64)
-	if err != nil {
-		c.JSON(
-			http.StatusBadRequest,
-			gin.H{"success": false, "reason": fmt.Errorf("invalid to time %s", err.Error())},
+			gin.H{"error": err.Error()},
 		)
 		return
 	}
 
-	fromTime := time.Unix(from/1000, 0)
-	toTime := time.Unix(to/1000, 0)
+	fromTime := time.Unix(0, int64(query.From)*int64(time.Millisecond))
+	toTime := time.Unix(0, int64(query.To)*int64(time.Millisecond))
+
+	if toTime.After(fromTime.Add(limitedTimeRange)) {
+		err := fmt.Errorf("time range is too broad, must be smaller or equal to %d milliseconds", limitedTimeRange/time.Millisecond)
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": err.Error()},
+		)
+		return
+	}
+
+	if toTime.Equal(time.Unix(0, 0)) {
+		toTime = time.Now()
+		fromTime = toTime.Add(-time.Hour)
+	}
+
 	tradeLogs, err := ha.storage.LoadTradeLogs(fromTime, toTime)
 	if err != nil {
 		c.JSON(
-			http.StatusOK,
-			gin.H{"success": false, "reason": fmt.Errorf("invalid to time %s", err.Error())},
+			http.StatusInternalServerError,
+			gin.H{"error": err.Error()},
 		)
 		return
 	}
 
 	c.JSON(
 		http.StatusOK,
-		gin.H{"success": true, "data": tradeLogs},
+		tradeLogs,
 	)
 }
 
 // Start running http server to serve trade logs data
-func (ha *Server) Start() {
+func (ha *Server) Start() error {
 	ha.router.GET("/trade-logs", ha.getTradeLogs)
-	ha.router.Run(ha.addr)
+
+	return ha.router.Run(ha.addr)
 }
 
 // NewServer returns an instance of HttpApi to serve trade logs

a50420073cf9f7b6932a3af70408bf53a794bb08
Diep Pham <me@favadi.com>
2018-10-24 09:58:55 +0700

    minor style fixes

--- users/storage/influx.go
+++ users/storage/influx.go
@@ -44,34 +44,35 @@ func (inf *InfluxStorage) queryDB(clnt client.Client, cmd string) (res []client.
 
 //IsExceedDailyLimit return if add address trade over daily limit or not
 func (inf *InfluxStorage) IsExceedDailyLimit(address string, dailyLimit float64) (bool, error) {
-	query := fmt.Sprintf(`SELECT SUM(amount) as daily_fiat_amount FROM (SELECT eth_amount*eth_usd_rate as amount 
+	var (
+		query = fmt.Sprintf(`SELECT SUM(amount) as daily_fiat_amount FROM (SELECT eth_amount*eth_usd_rate as amount 
 FROM trades WHERE user_addr='%s' AND time <= now() AND time >= (now()-24h))`,
-		address)
-	res, err := inf.queryDB(inf.influxClient, query)
+			address)
+		userTradeAmount float64
+		ok              bool
+	)
 
+	res, err := inf.queryDB(inf.influxClient, query)
 	if err != nil {
 		inf.sugar.Debugw("error from query", "error", err)
 		return false, err
 	}
 	inf.sugar.Debugw("result from query", "result", res)
 
-	var userTradeAmount float64
-	var ok bool
-	if len(res[0].Series) > 0 {
-		if len(res[0].Series[0].Values) > 0 {
-			userTradeAmount, ok = (res[0].Series[0].Values[0][1]).(float64)
-			if !ok {
-				inf.sugar.Debugw("values second should be float", "value", res[0].Series[0].Values[0][1])
-				return false, errors.New("trade amount values is not a float")
-			}
-		} else {
-			inf.sugar.Debugw("return values from influx", "values", res[0].Series[0].Values)
-			return false, nil
-		}
-	} else {
+	if len(res) == 0 || len(res[0].Series) == 0 || len(res[0].Series[0].Values) == 0 || len(res[0].Series[0].Values[0]) < 2 {
 		inf.sugar.Debugw("user address was not found from trade db", "user address", address)
 		return false, nil
 	}
-	inf.sugar.Debugw("user rich is", "address", address, "user trade amount", userTradeAmount, "daily limit", dailyLimit)
+
+	userTradeAmount, ok = (res[0].Series[0].Values[0][1]).(float64)
+	if !ok {
+		inf.sugar.Debugw("values second should be float", "value", res[0].Series[0].Values[0][1])
+		return false, errors.New("trade amount values is not a float")
+	}
+
+	inf.sugar.Debugw("got last 24h total transaction",
+		"address", address,
+		"user trade amount", userTradeAmount,
+		"daily limit", dailyLimit)
 	return userTradeAmount >= dailyLimit, nil
 }

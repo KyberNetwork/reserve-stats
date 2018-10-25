7d755f13057fa789a3d6fac70fa84c9943bd467c
Diep Pham <diep@kyber.network>
2018-10-25 17:35:03 +0700

    add export.sh script for automatically export tradelogs sample data

---
+++ tradelogs/storage/testdata/export.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# -*- firestarter: "shfmt -i 4 -ci -w %p" -*-
+# Usage: ./export.sh <core-url> <core-singing-key>
+
+set -euo pipefail
+
+readonly core_url=${1:-}
+readonly core_signing_key=${2:-}
+
+readonly from_block=6494037
+readonly to_block=6494137
+readonly influxdb_image='influxdb:1.6.3-alpine'
+readonly container_name='trade-logs-sample-data-exporter'
+readonly data_dir='/tmp/trade-logs-sample-data-exporter'
+
+if [[ -z "$core_url" ]]; then
+    echo "missing core url"
+    exit 1
+fi
+
+if [[ -z "$core_signing_key" ]]; then
+    echo "missing core signing key"
+    exit 1
+fi
+
+docker run --rm --detach --name "$container_name" \
+    --volume "$data_dir:/var/lib/influxdb/" \
+    --publish '127.0.0.1:8087:8086' \
+    "$influxdb_image"
+
+sleep 5
+
+pushd ../../cmd/trade-logs-crawler
+go build
+./trade-logs-crawler --influxdb-endpoint=http://127.0.0.1:8087 \
+    --core-url "$core_url" --core-signing-key "$core_signing_key" \
+    --from-block "$from_block" --to-block "$to_block"
+popd
+
+docker exec "$container_name" influx_inspect export -datadir /var/lib/influxdb/data \
+    -waldir /var/lib/influxdb/wal -out /var/lib/influxdb/data/export.dat \
+    -database trade_logs -retention autogen
+
+cp "$data_dir/data/export.dat" export.dat
+sed -i -e '/^#/d' export.dat
+
+docker kill "$container_name"
+sudo rm -rf "$data_dir"

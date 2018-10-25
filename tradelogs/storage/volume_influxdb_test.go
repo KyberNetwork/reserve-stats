ca3d4149f6b1ddd768b16c2557a6f477be86e7a9
kyberliem <cyberliem.civil@gmail.com>
2018-10-25 10:45:07 +0700

    use influx client instead of http query

--- tradelogs/storage/volume_influxdb_test.go
+++ tradelogs/storage/volume_influxdb_test.go
@@ -8,7 +8,6 @@ import (
 	"time"
 
 	"github.com/KyberNetwork/reserve-stats/lib/core"
-	// "github.com/KyberNetwork/reserve-stats/lib/timeutil"
 )
 
 func doInfluxHTTPReq(client http.Client, cmd, endpoint, db string) error {
@@ -30,7 +29,7 @@ func doInfluxHTTPReq(client http.Client, cmd, endpoint, db string) error {
 	return nil
 }
 
-func aggregationTestData(db string) error {
+func aggregationTestData(is *InfluxStorage) error {
 	const (
 		DstAmountHourCmd = "SELECT SUM(dst_amount) AS token_volume, SUM(eth_amount) AS eth_volume, SUM(usd_amount) AS usd_volume INTO volume_hour FROM (SELECT dst_amount, eth_amount, eth_amount*eth_usd_rate AS usd_amount FROM trades WHERE (src_addr!='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' AND dst_addr!='0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2') OR (src_addr!='0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' AND dst_addr!='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE')) GROUP BY \"dst_addr\", time(1h)"
 		DstAmountDayCmd  = "SELECT SUM(token_volume) as token_volume, SUM(eth_volume) as eth_volume, SUM(usd_volume) AS usd_volume INTO volume_day FROM volume_hour WHERE dst_addr!='' GROUP BY \"dst_addr\", time(1d)"
@@ -38,65 +37,62 @@ func aggregationTestData(db string) error {
 		SrcAmountDayCmd  = "SELECT SUM(token_volume) as token_volume, SUM(eth_volume) as eth_volume, SUM(usd_volume) AS usd_volume INTO volume_day FROM volume_hour WHERE src_addr!=''GROUP BY \"src_addr\", time(1d)"
 		endpoint         = "http://127.0.0.1:8086/"
 	)
-	client := http.Client{Timeout: time.Second * 5}
-	if err := doInfluxHTTPReq(client, DstAmountHourCmd, endpoint+"query", db); err != nil {
-		log.Printf("fuck")
+	if _, err := is.queryDB(is.influxClient, DstAmountHourCmd); err != nil {
 		return err
 	}
-	if err := doInfluxHTTPReq(client, DstAmountDayCmd, endpoint+"query", db); err != nil {
+	if _, err := is.queryDB(is.influxClient, DstAmountDayCmd); err != nil {
 		return err
 	}
-	if err := doInfluxHTTPReq(client, SrcAmountHourCmd, endpoint+"query", db); err != nil {
+	if _, err := is.queryDB(is.influxClient, SrcAmountHourCmd); err != nil {
 		return err
 	}
-	if err := doInfluxHTTPReq(client, SrcAmountDayCmd, endpoint+"query", db); err != nil {
+	if _, err := is.queryDB(is.influxClient, SrcAmountDayCmd); err != nil {
 		return err
 	}
 	return nil
 }
 
 func TestGetAssetVolume(t *testing.T) {
-	const dbName = "test_volume"
+	const (
+		dbName = "test_volume"
+		// These params are expected to be change when export.dat changes.
+		fromTime   = 1539248043000
+		toTime     = 1539248666000
+		eth_amount = 238.33849929550047
+		freq       = "h"
+		timeStamp  = "2018-10-11T09:00:00Z"
+	)
+
 	is, err := newTestInfluxStorage(dbName)
-	// defer func() {
-	// 	if err := is.tearDown(); err != nil {
-	// 		t.Fatal(err)
-	// 	}
-	// }()
+	defer func() {
+		if err := is.tearDown(); err != nil {
+			t.Fatal(err)
+		}
+	}()
 	if err := loadTestData(dbName); err != nil {
 		t.Fatal(err)
 	}
-
-	if err := aggregationTestData(dbName); err != nil {
+	if err := aggregationTestData(is); err != nil {
 		t.Fatal(err)
 	}
 	if err != nil {
 		t.Fatal(err)
 	}
-	// 2018-10-11T16:00:00+07:00
-	// These number is expected to be change when export.dat changes.
-	volume, err := is.GetAssetVolume(core.ETHToken, 1539248043000, 1539248666000, "h")
+	volume, err := is.GetAssetVolume(core.ETHToken, fromTime, toTime, freq)
 	log.Printf("%v", volume)
 	if err != nil {
 		t.Fatal(err)
 	}
-	timeStamp, err := time.Parse(time.RFC3339, "2018-10-11T09:00:00Z")
+	timeUnix, err := time.Parse(time.RFC3339, timeStamp)
 	if err != nil {
 		t.Fatal(err)
 	}
-	result, ok := volume[timeStamp]
+	result, ok := volume[timeUnix]
 	if !ok {
-		t.Fatalf("expect to find result at timestamp %s, yet there is none", timeStamp.Format(time.RFC3339))
+		t.Fatalf("expect to find result at timestamp %s, yet there is none", timeUnix.Format(time.RFC3339))
 	}
 
-	if result.USDAmount != 238.33849929550047 {
-		t.Fatal(fmt.Errorf("Expect USD amount to be 238.33849929550047, got %.f", result.USDAmount))
+	if result.USDAmount != eth_amount {
+		t.Fatal(fmt.Errorf("Expect USD amount to be %.18f, got %.18f", eth_amount, result.USDAmount))
 	}
 }
-
-// SELECT SUM(token_volume) as token_volume, SUM(eth_volume) as eth_volume, sum(usd_volume) as usd_volume FROM volume_hour WHERE (dst_addr='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' OR src_addr='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE') GROUP BY time(1h) fill(none)
-// name: volume_hour
-// time                 token_volume       eth_volume         usd_volume
-// ----                 ------------       ----------         ----------
-// 2018-10-11T08:00:00Z 16.333613369193138 16.333613369193138 3685.8761243552653
-// 2018-10-11T09:00:00Z 1.0561746426481893 1.0561746426481893 238.33849929550047

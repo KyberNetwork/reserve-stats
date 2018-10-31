d7a1978a9fbcae43fc555876f49b4c413940032a
ductm54 <ductm54@gmail.com>
2018-10-31 15:26:39 +0700

    add httpsign client

---
+++ lib/httputil/httprequestsign.go
@@ -0,0 +1,100 @@
+package httputil
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+)
+
+const (
+	requestTarget = "(request-target)"
+	date          = "date"
+	digest        = "digest"
+	host          = "host"
+)
+
+//TODO: add signing algorithm
+
+//Sign sign a http request with selected header and return a signed request
+//Use with http client only
+func Sign(r *http.Request, headers []string, keyID string, secret string) (*http.Request, error) {
+	if len(headers) == 0 {
+		headers = []string{"date"}
+	}
+	var signBuffer bytes.Buffer
+	for i, h := range headers {
+		var value string
+		switch h {
+		case digest:
+			value, err := calculateDigest(r)
+			if err != nil {
+				return nil, err
+			}
+			r.Header.Set(h, value)
+		case date:
+			value = time.Now().UTC().Format(http.TimeFormat)
+			r.Header.Set(h, value)
+		case requestTarget:
+			value = fmt.Sprintf("%s %s", strings.ToLower(r.Method), r.URL.RequestURI())
+		case host:
+			value = r.Host
+		default:
+			value = r.Header.Get(h)
+		}
+		signString := fmt.Sprintf("%s: %s", h, value)
+		signBuffer.WriteString(signString)
+		if i < len(headers)-1 {
+			signBuffer.WriteString("\n")
+		}
+	}
+	signString := signBuffer.String()
+	signature, err := sign(signString, secret)
+	if err != nil {
+		return nil, err
+	}
+	signatureHeader := constructHeader(headers, keyID, signature)
+	r.Header.Set("Signature", signatureHeader)
+	return r, nil
+}
+
+func calculateDigest(r *http.Request) (string, error) {
+	if r.Body == nil {
+		return "", nil
+	}
+	body, err := r.GetBody()
+	if err != nil {
+		return "", err
+	}
+	h := sha256.New()
+	_, err = io.Copy(h, body)
+	if err != nil {
+		return "", err
+	}
+	digest := fmt.Sprintf("SHA-256=%s", base64.StdEncoding.EncodeToString(h.Sum(nil)))
+	return digest, nil
+}
+
+func sign(msg string, secret string) (string, error) {
+	mac := hmac.New(sha512.New, []byte(secret))
+	if _, err := mac.Write([]byte(msg)); err != nil {
+		return "", err
+	}
+	signature := base64.StdEncoding.EncodeToString(mac.Sum(nil))
+	return signature, nil
+}
+
+func constructHeader(headers []string, keyID string, signature string) string {
+	var signBuffer bytes.Buffer
+	signBuffer.WriteString(fmt.Sprintf(`keyId="%s",`, keyID))
+	signBuffer.WriteString(`algorithm="hmac-sha512"`)
+	signBuffer.WriteString(fmt.Sprintf(`header="%s"`, strings.Join(headers, " ")))
+	signBuffer.WriteString(fmt.Sprintf(`signature="%s"`, signature))
+	return signBuffer.String()
+}

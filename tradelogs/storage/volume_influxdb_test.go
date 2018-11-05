c78bb8211c71ebd512f292d5bd7f80f8615c154a
kyberliem <cyberliem.civil@gmail.com>
2018-11-05 15:49:56 +0700

    remove unused file

--- tradelogs/storage/volume_influxdb_test.go
+++ tradelogs/storage/volume_influxdb_test.go
@@ -49,31 +49,6 @@ func aggregationTestData(is *InfluxStorage) error {
 }
 
 func aggregationVolumeTestData(is *InfluxStorage) error {
-	// var (
-	// 	hourCmds = []string{
-	//	"SELECT SUM(src_amount) AS token_volume, SUM(eth_amount) AS eth_volume, SUM(usd_amount) AS usd_volume INTO rsv_volume_hour FROM (SELECT src_amount, eth_amount, eth_amount*eth_usd_rate AS usd_amount FROM trades WHERE (src_addr!='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' AND dst_addr!='0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2') OR (src_addr!='0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' AND dst_addr!='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE') AND src_rsv_addr!='') GROUP BY src_addr,src_rsv_addr,time(1h)"
-	// 		"SELECT SUM(src_amount) AS token_volume, SUM(eth_amount) AS eth_volume, SUM(usd_amount) AS usd_volume INTO rsv_volume_hour FROM (SELECT src_amount, eth_amount, eth_amount*eth_usd_rate AS usd_amount FROM trades WHERE (src_addr!='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' AND dst_addr!='0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2') OR (src_addr!='0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' AND dst_addr!='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE') AND src_rsv_addr!='') GROUP BY src_addr,src_rsv_addr,time(1h)",
-	// 		"SELECT SUM(src_amount) AS token_volume, SUM(eth_amount) AS eth_volume, SUM(usd_amount) AS usd_volume INTO rsv_volume_hour FROM (SELECT src_amount, eth_amount, eth_amount*eth_usd_rate AS usd_amount FROM trades WHERE (src_addr!='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' AND dst_addr!='0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2') OR (src_addr!='0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' AND dst_addr!='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE') AND dst_rsv_addr!='') GROUP BY src_addr,dst_rsv_addr,time(1h)",
-	// 		"SELECT SUM(dst_amount) AS token_volume, SUM(eth_amount) AS eth_volume, SUM(usd_amount) AS usd_volume INTO rsv_volume_hour FROM (SELECT dst_amount, eth_amount, eth_amount*eth_usd_rate AS usd_amount FROM trades WHERE (src_addr!='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' AND dst_addr!='0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2') OR (src_addr!='0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' AND dst_addr!='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE') AND src_rsv_addr!='') GROUP BY dst_addr,src_rsv_addr,time(1h)",
-	// 		"SELECT SUM(dst_amount) AS token_volume, SUM(eth_amount) AS eth_volume, SUM(usd_amount) AS usd_volume INTO rsv_volume_hour FROM (SELECT dst_amount, eth_amount, eth_amount*eth_usd_rate AS usd_amount FROM trades WHERE (src_addr!='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' AND dst_addr!='0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2') OR (src_addr!='0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' AND dst_addr!='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE') AND dst_rsv_addr!='') GROUP BY dst_addr,dst_rsv_addr,time(1h)",
-	// 	}
-	// 	dayCmds = []string{
-	// 		"SELECT SUM(token_volume) as token_volume, SUM(eth_volume) AS eth_volume, SUM(usd_volume) AS usd_volume INTO rsv_volume_day FROM rsv_volume_hour WHERE src_addr!='' AND src_rsv_addr!='' GROUP BY src_addr,src_rsv_addr,time(1d)",
-	// 		"SELECT SUM(token_volume) as token_volume, SUM(eth_volume) as eth_volume, SUM(usd_volume) AS usd_volume INTO rsv_volume_day FROM rsv_volume_hour WHERE src_addr!='' AND dst_rsv_addr!='' GROUP BY src_addr,dst_rsv_addr,time(1d)",
-	// 		"SELECT SUM(token_volume) as token_volume, SUM(eth_volume) as eth_volume, SUM(usd_volume) AS usd_volume INTO rsv_volume_day FROM rsv_volume_hour WHERE dst_addr!='' and src_rsv_addr!='' GROUP BY dst_addr,src_rsv_addr,time(1d)",
-	// 		"SELECT SUM(token_volume) as token_volume, SUM(eth_volume) as eth_volume, SUM(usd_volume) AS usd_volume INTO rsv_volume_day FROM rsv_volume_hour WHERE dst_addr!='' and dst_rsv_addr!='' GROUP BY dst_addr,dst_rsv_addr,time(1d)",
-	// 	}
-	// )
-	// for _, cmd := range hourCmds {
-	// 	if _, err := is.queryDB(is.influxClient, cmd); err != nil {
-	// 		return err
-	// 	}
-	// }
-	// for _, cmd := range dayCmds {
-	// 	if _, err := is.queryDB(is.influxClient, cmd); err != nil {
-	// 		return err
-	// 	}
-	// }
 	cqs, err := tradelogcq.CreateReserveVolumeCqs(is.dbName)
 	if err != nil {
 		return err

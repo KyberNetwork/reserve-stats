07a80b8c746242811f1e55a9fa34a07e6c7bc62e
Hoàng Hà <halink0803@gmail.com>
2018-11-05 17:00:01 +0700

    add price analytic data api

--- price-analytics/http/server.go
+++ price-analytics/http/server.go
@@ -1,10 +1,108 @@
 package http
 
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/KyberNetwork/reserve-stats/lib/timeutil"
+	"github.com/KyberNetwork/reserve-stats/price-analytics/common"
+	"github.com/KyberNetwork/reserve-stats/price-analytics/storage"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
 //Server for price analytic service
 type Server struct {
+	sugar   *zap.SugaredLogger
+	r       *gin.Engine
+	host    string
+	storage storage.Interface
 }
 
 //NewHTTPServer return new server instance
-func NewHTTPServer() {
+func NewHTTPServer(sugar *zap.SugaredLogger, host string, storage storage.Interface) *Server {
+	r := gin.Default()
+	return &Server{
+		sugar:   sugar,
+		r:       r,
+		host:    host,
+		storage: storage,
+	}
+}
 
+func (s *Server) updatePriceAnalytic(c *gin.Context) {
+	var priceAnalytic common.PriceAnalytic
+	if err := c.ShouldBindJSON(&priceAnalytic); err != nil {
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{},
+		)
+		return
+	}
+	if err := s.storage.UpdatePriceAnalytic(priceAnalytic); err != nil {
+		c.JSON(
+			http.StatusInternalServerError,
+			gin.H{},
+		)
+		return
+	}
+	c.JSON(
+		http.StatusOK,
+		gin.H{},
+	)
+}
+
+func (s *Server) validateTimeInput(c *gin.Context) (time.Time, time.Time, bool) {
+	var (
+		from, to time.Time
+	)
+	fromTime, ok := strconv.ParseUint(c.Query("fromTime"), 10, 64)
+	if ok != nil {
+		return from, to, false
+	}
+	from = timeutil.TimestampMsToTime(fromTime)
+	toTime, _ := strconv.ParseUint(c.Query("toTime"), 10, 64)
+	if toTime == 0 {
+		to = time.Now()
+	} else {
+		to = timeutil.TimestampMsToTime(toTime)
+	}
+	return from, to, true
+}
+
+func (s *Server) getPriceAnalytic(c *gin.Context) {
+	fromTime, toTime, ok := s.validateTimeInput(c)
+	if !ok {
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{
+				"error": "time input is not valid",
+			},
+		)
+		return
+	}
+	priceAnalytic, err := s.storage.GetPriceAnalytic(fromTime, toTime)
+	if err != nil {
+		c.JSON(
+			http.StatusInternalServerError,
+			gin.H{},
+		)
+		return
+	}
+	c.JSON(
+		http.StatusOK,
+		priceAnalytic,
+	)
+}
+
+func (s *Server) register() {
+	s.r.POST("/price-analytic-data", s.updatePriceAnalytic)
+	s.r.GET("/price-analytic-data", s.getPriceAnalytic)
+}
+
+// Run server
+func (s *Server) Run() error {
+	s.register()
+	return s.r.Run(s.host)
 }
--- price-analytics/storage/price_analytic_storage.go
+++ price-analytics/storage/price_analytic_storage.go
@@ -1,10 +1,23 @@
 package storage
 
-import "github.com/go-pg/pg"
+import (
+	"fmt"
+	"time"
+
+	"github.com/KyberNetwork/reserve-stats/price-analytics/common"
+	"github.com/jmoiron/sqlx"
+	"go.uber.org/zap"
+)
+
+const (
+	priceAnalyticTableName     = "price_analytics"
+	priceAnalyticDataTableName = "price_analytics_data"
+)
 
 //PriceAnalyticDB represent db for price analytic data
 type PriceAnalyticDB struct {
-	db *pg.DB
+	sugar *zap.SugaredLogger
+	db    *sqlx.DB
 }
 
 //StorePriceAnalytic store information from send from analytic to stat
@@ -13,6 +26,113 @@ func (pad *PriceAnalyticDB) StorePriceAnalytic() error {
 }
 
 //NewPriceStorage return new storage for price analytics
-func NewPriceStorage() {
+func NewPriceStorage(sugar *zap.SugaredLogger, db *sqlx.DB) (*PriceAnalyticDB, error) {
+	const schemaFmt = `
+CREATE TABLE IF NOT EXISTS "%s" (
+	id SERIAL PRIMARY KEY,
+	timestamp TIMESTAMP NOT NULL,
+	block_expriration BOOL NOT NULL
+);
 
+CREATE TABLE IF NOT EXISTS "%s" (
+	id SERIAL PRIMARY KEY,
+	token text NOT NULL,
+	ask_price float NOT NULL,
+	bid_price float NOT NULL,
+	min_afp_price float NOT NULL,
+	min_afp_old_price float NOT NULL,
+	min_spread float NOT NULL,
+	trigger_update bool NOT NULL,
+	price_analatic_id NOT NULL REFERENCES "%s" (id)
+);
+`
+	var logger = sugar.With("func", "price-analytics/storage.NewPriceStorage")
+
+	tx, err := db.Beginx()
+	if err != nil {
+		return nil, err
+	}
+
+	logger.Debug("initilizing database schema")
+
+	if _, err = tx.Exec(fmt.Sprintf(schemaFmt, priceAnalyticTableName, priceAnalyticDataTableName, priceAnalyticTableName)); err != nil {
+		return nil, err
+	}
+	logger.Debug("database schema initilized successfully")
+
+	return &PriceAnalyticDB{
+		sugar: sugar,
+		db:    db,
+	}, nil
+}
+
+// UpdatePriceAnalytic store price analytic to db
+func (pad *PriceAnalyticDB) UpdatePriceAnalytic(data common.PriceAnalytic) error {
+	var (
+		logger = pad.sugar.With(
+			"func", "price-analytics/storage.UpdatePriceAnalytic",
+		)
+		priceAnalyticID int
+	)
+
+	tx, err := pad.db.Beginx()
+	if err != nil {
+		return err
+	}
+	// insert into price_analytics
+	row := tx.QueryRowx(`INSERt INTO price_analytics (timestamp, block_expiration) 
+	VALUES ((TO_TIMESTAMP($1::double precision/1000)), $2) RETURNING id;`, data.Timestamp, data.BlockExpiration)
+	if err = row.Scan(&priceAnalyticID); err != nil {
+		return err
+	}
+	logger = logger.With("price_analytic_id", priceAnalyticID)
+	logger.Debug("price analytic data created")
+
+	// insert into price_analytic_data
+	for _, values := range data.TriggeringTokensList {
+		_, err = tx.Exec(fmt.Sprintf(`
+INSERT INTO "%s" (token, ask_price, bid_price, min_afp_price, min_afp_old_price, min_spread, trigger_update, price_analytic_id)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+		`, priceAnalyticDataTableName),
+			values.Token,
+			values.AskPrice,
+			values.BidPrice,
+			values.MinAfpPrice,
+			values.MinAfpOldPrice,
+			values.MinSpread,
+			values.TriggerUpdate,
+			priceAnalyticID,
+		)
+		if err != nil {
+			return err
+		}
+	}
+
+	return tx.Commit()
+}
+
+// GetPriceAnalytic get price analytic data to return to api
+func (pad *PriceAnalyticDB) GetPriceAnalytic(fromTime, toTime time.Time) ([]common.PriceAnalytic, error) {
+	var (
+		result []common.PriceAnalytic
+		err    error
+	)
+
+	if err := pad.db.Get(&result, fmt.Sprintf(`SELECT * FROM "%s" WHERE timestamp >= $1 AND timestamp <= $2`, priceAnalyticTableName), fromTime, toTime); err != nil {
+		return result, err
+	}
+
+	if len(result) > 0 {
+		for index, data := range result {
+			priceAnalyticData := []common.PriceAnalyticData{}
+			if err := pad.db.Get(&priceAnalyticData, fmt.Sprintf(`SELECT * from "%s" WHERE price_analytic_id = $1`, priceAnalyticDataTableName), data.ID); err != nil {
+				return result, err
+			}
+			if len(priceAnalyticData) > 0 {
+				result[index].TriggeringTokensList = priceAnalyticData
+			}
+		}
+	}
+
+	return result, err
 }

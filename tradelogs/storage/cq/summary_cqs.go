a890dc4b4a13791c38e98b143e5d45c45054ed32
Hoàng Hà <halink0803@gmail.com>
2018-11-14 11:04:58 +0700

    rebase develop

--- tradelogs/storage/cq/summary_cqs.go
+++ tradelogs/storage/cq/summary_cqs.go
@@ -27,7 +27,11 @@ func CreateSummaryCqs(dbName string) ([]*libcq.ContinuousQuery, error) {
 		dbName,
 		dayResampleInterval,
 		dayResampleFor,
-		"SELECT SUM(eth_amount) AS total_eth_volume, SUM(usd_amount) AS total_usd_amount, COUNT(eth_amount) AS total_trade, MEAN(usd_amount) AS usd_per_trade, MEAN(eth_amount) AS eth_per_trade INTO trade_summary FROM (SELECT dst_amount, eth_amount, eth_amount*eth_usd_rate AS usd_amount FROM trades WHERE (src_addr!='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' AND dst_addr!='0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2') OR (src_addr!='0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' AND dst_addr!='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'))",
+		"SELECT SUM(eth_amount) AS total_eth_volume, SUM(usd_amount) AS total_usd_amount, COUNT(eth_amount) AS total_trade, "+
+			"MEAN(usd_amount) AS usd_per_trade, MEAN(eth_amount) AS eth_per_trade INTO trade_summary "+
+			"FROM (SELECT dst_amount, eth_amount, eth_amount*eth_usd_rate AS usd_amount FROM trades "+
+			"WHERE (src_addr!='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' AND dst_addr!='0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2') "+
+			"OR (src_addr!='0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' AND dst_addr!='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'))",
 		"1d",
 		[]string{},
 	)

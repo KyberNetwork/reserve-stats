463899035fa8aa2da76b5329fbb48e92818959d2
kyberliem <cyberliem.civil@gmail.com>
2018-11-15 17:41:24 +0700

    add tradelog schema

---
+++ tradelogs/storage/schema/tradelog_schema.go
@@ -0,0 +1,60 @@
+package schema
+
+// TradeLogSchemaFieldName define a list of field names for a TradeLog record
+//go:generate stringer -type=TradeLogSchemaFieldName -linecomment
+type TradeLogSchemaFieldName int
+
+const (
+	//BlockNumber is enumerated field name for TradeLog.BlockNumbers
+	BlockNumber TradeLogSchemaFieldName = iota //block_number
+	//TxHash is enumerated field name for tradeLog.TxHash
+	TxHash //tx_hash
+	//EthReceivalSender is enumerated field name for TradeLog.EtherReceivalSender
+	EthReceivalSender //eth_receival_sender
+	//UserAddr is enumerated field name for TradeLog.UserAddr
+	UserAddr //user_addr
+	//SrcAddr is enumerated field name for TradeLog.SrcAddr
+	SrcAddr //src_addr
+	//DstAddr is enumerated field name for TradeLog.DstAddr
+	DstAddr //dst_addr
+	//Country is enumerated field name for TradeLog.CountryName
+	Country //country
+	//IP is enumerated field name for TradeLog.IP
+	IP //ip
+	//EthUSDProvider is the enumerated field name for TradeLog.ETHUSDProvider
+	EthUSDProvider //eth_rate_provider
+	//DstReserveAddr is enumerated fieldname for destination reserve Address
+	DstReserveAddr // dst_rsv_addrs
+	//SrcReserveAddr is enumerated field for source reserve Address
+	SrcReserveAddr // src_rsv_addrs
+	//EthReceivalAmount is the enumerated field for ETHReceivalAmount
+	EthReceivalAmount // eth_receival_amount
+	//SrcAmount is the enumerated field for source amount
+	SrcAmount //src_amount
+	//DstAmount is the enumerated field for source amount
+	DstAmount //dst_amount
+	//EthUSDRate is the enumerated field for ETH-USD rate
+	EthUSDRate // eth_usd_rate
+	//EthAmount is the enumerated field for ETH Amount
+	EthAmount // eth_amount
+)
+
+//tradeLogSchemaFields translates the stringer of reserveRate fields into its enumerated form
+var tradeLogSchemaFields = map[string]TradeLogSchemaFieldName{
+	"block_number":        BlockNumber,
+	"tx_hash":             TxHash,
+	"eth_receival_sender": EthReceivalSender,
+	"user_addr":           UserAddr,
+	"src_addr":            SrcAddr,
+	"dst_addr":            DstAddr,
+	"country":             Country,
+	"ip":                  IP,
+	"eth_rate_provider":   EthUSDProvider,
+	"dst_rsv_addr":        DstReserveAddr,
+	"src_rsv_addr":        SrcReserveAddr,
+	"eth_receival_amount": EthReceivalAmount,
+	"src_amount":          SrcAmount,
+	"dst_amount":          DstAmount,
+	"eth_usd_rate":        EthUSDRate,
+	"eth_amount":          EthAmount,
+}

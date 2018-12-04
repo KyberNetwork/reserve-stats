e3beed30b75398de9e61fc95e130594ffc428827
Hoàng Hà <halink0803@gmail.com>
2018-12-04 17:39:00 +0700

    update docker compose

--- init-postgres-db.sh
+++ init-postgres-db.sh
@@ -3,5 +3,5 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE users;
-    CREATE DATABASE price
+    CREATE DATABASE "price-analytics";
 EOSQL

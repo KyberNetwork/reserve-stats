7d5981b8dbacca332a106ef264ab23fd6e2fe040
Hoàng Hà <halink0803@gmail.com>
2018-12-06 10:58:12 +0700

    update integration app

--- app-names/http/server.go
+++ app-names/http/server.go
@@ -2,55 +2,76 @@ package http
 
 import (
 	"net/http"
+	"strconv"
 
-	appNames "github.com/KyberNetwork/reserve-stats/app-names"
+	"github.com/KyberNetwork/reserve-stats/app-names/common"
+	"github.com/KyberNetwork/reserve-stats/app-names/storage"
 	"github.com/KyberNetwork/reserve-stats/lib/httputil"
 	_ "github.com/KyberNetwork/reserve-stats/lib/httputil/validators" // import custom validator functions
-	ethereum "github.com/ethereum/go-ethereum/common"
+	// ethereum "github.com/ethereum/go-ethereum/common"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
 
 // Server is the engine to serve reserve-rate API query
 type Server struct {
-	r             *gin.Engine
-	host          string
-	sugar         *zap.SugaredLogger
-	addrToAppName appNames.AddrToAppName
+	r     *gin.Engine
+	host  string
+	sugar *zap.SugaredLogger
+	db    *storage.AppNameDB
 }
 
-// AddrNameQuery define a struct to contain param for a POST add-to-appname
-type AddrNameQuery struct {
-	Address string `json:"address" binding:"required,isAddress"`
-	AppName string `json:"appname" binding:"required"`
-}
-
-func (sv *Server) getAddrToAppName(c *gin.Context) {
+func (sv *Server) getApps(c *gin.Context) {
 	var (
 		logger = sv.sugar.With("func", "intergration-app-names/http/Server.getAddrToAppName")
 	)
 
 	logger.Debug("getting addr to App name")
-	result := sv.addrToAppName.GetAddrToAppName()
+}
+
+func (sv *Server) getAddressFromAppID(c *gin.Context) {
+	appIDStr := c.Param("appID")
+	appID, err := strconv.ParseInt(appIDStr, 10, 64)
+	if err != nil {
+		httputil.ResponseFailure(
+			c,
+			http.StatusBadRequest,
+			err,
+		)
+		return
+	}
+
+	result, err := sv.db.GetAppAddresses(appID)
+	if err != nil {
+		httputil.ResponseFailure(
+			c,
+			http.StatusInternalServerError,
+			err,
+		)
+		return
+	}
+
 	c.JSON(http.StatusOK, result)
 }
 
 func (sv *Server) updateAddrToAppName(c *gin.Context) {
 	var (
-		logger = sv.sugar.With("func", "intergration-app-names/http/Server.updateAddrToAppName")
-		q      AddrNameQuery
+		logger   = sv.sugar.With("func", "intergration-app-names/http/Server.updateAddrToAppName")
+		q        common.AppObject
+		response common.AppObject
+		err      error
 	)
 
 	logger.Debug("updating addr to App name")
 	if err := c.ShouldBindJSON(&q); err != nil {
 		httputil.ResponseFailure(
 			c,
-			http.StatusInternalServerError,
+			http.StatusBadRequest,
 			err,
 		)
 		return
 	}
-	if err := sv.addrToAppName.UpdateMapAddrAppName(ethereum.HexToAddress(q.Address), q.AppName); err != nil {
+	if response, err = sv.db.CreateOrUpdate(q); err != nil {
 		httputil.ResponseFailure(
 			c,
 			http.StatusInternalServerError,
@@ -58,12 +79,13 @@ func (sv *Server) updateAddrToAppName(c *gin.Context) {
 		)
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{q.Address: q.AppName})
+	c.JSON(http.StatusOK, response)
 }
 
 func (sv *Server) register() {
-	sv.r.GET("/application-names", sv.getAddrToAppName)
-	sv.r.POST("/application-names", sv.updateAddrToAppName)
+	sv.r.GET("/application-names", sv.getApps)
+	sv.r.GET("/application-names/:appID", sv.getAddressFromAppID)
+	sv.r.POST("/application-name", sv.updateAddrToAppName)
 }
 
 // Run starts HTTP server on preconfigure-host. Return error if occurs
@@ -73,12 +95,12 @@ func (sv *Server) Run() error {
 }
 
 // NewServer create an instance of Server to serve API query
-func NewServer(host string, atan appNames.AddrToAppName, sugar *zap.SugaredLogger) (*Server, error) {
+func NewServer(host string, appNameDB *storage.AppNameDB, sugar *zap.SugaredLogger) (*Server, error) {
 	r := gin.Default()
 	return &Server{
-		r:             r,
-		addrToAppName: atan,
-		host:          host,
-		sugar:         sugar,
+		r:     r,
+		db:    appNameDB,
+		host:  host,
+		sugar: sugar,
 	}, nil
 }

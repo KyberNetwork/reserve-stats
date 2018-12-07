3dca07fbbbe5420613d33e9d07601b03251a0579
Hoàng Hà <halink0803@gmail.com>
2018-12-07 18:09:33 +0700

    fix several errors

--- app-names/common/types.go
+++ app-names/common/types.go
@@ -1,8 +1,10 @@
 package common
 
+import "github.com/ethereum/go-ethereum/common"
+
 // AppObject an app
 type AppObject struct {
-	ID        int64    `json:"id,omitempty"`
-	AppName   string   `json:"app_name" binding:"required"`
-	Addresses []string `json:"addresses" binding:"required"`
+	ID        int64            `json:"id,omitempty"`
+	AppName   string           `json:"app_name" binding:"required"`
+	Addresses []common.Address `json:"addresses" binding:"required"`
 }

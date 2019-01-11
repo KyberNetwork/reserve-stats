9a7d497ed5d4e22a81fe061323fa599c4833fb28
kyberliem <cyberliem.civil@gmail.com>
2019-01-11 16:13:52 +0700

    fill 0 inbetween records

--- tradelogs/cmd/trade-logs-post-processor/main.go
+++ tradelogs/cmd/trade-logs-post-processor/main.go
@@ -216,7 +216,7 @@ func run(c *cli.Context) error {
 		}
 
 		query = fmt.Sprintf(
-			`SELECT * INTO %[1]s FROM %[1]s WHERE time >= '%[2]s' AND time < '%[3]s' FILL(0)`,
+			`SELECT * INTO %[1]s FROM %[1]s WHERE time >= '%[2]s' AND time < '%[3]s' GROUP BY * FILL(0)`,
 			storage.ReportMeasurement, beginOfLastMonth.Format(time.RFC3339), beginOfThisMonth.Format(time.RFC3339),
 		)
 		sugar.Debug("query ", query)

3678961d610fe2de3a53656a02ed8260f3411a10
Hoàng Hà <halink0803@gmail.com>
2019-01-15 11:57:33 +0700

    update ether receival sender as res

--- tradelogs/storage/influx.go
+++ tradelogs/storage/influx.go
@@ -341,7 +341,9 @@ func (is *InfluxStorage) tradeLogToPoint(log common.TradeLog) ([]*client.Point, 
 			logger.Warnw("unexpected burn fees", "got", log.BurnFees, "want", "1 burn fees (dst)")
 		}
 	} else {
-		if log.ReserveAddresses.SrcReserveAddress.Hex() != "0x0000000000000000000000000000000000000000" {
+		if log.EtherReceivalSender.Hex() != "0x0000000000000000000000000000000000000000" {
+			tags[logschema.SrcReserveAddr.String()] = log.EtherReceivalSender.Hex()
+		} else if log.ReserveAddresses.SrcReserveAddress.Hex() != "0x0000000000000000000000000000000000000000" {
 			tags[logschema.SrcReserveAddr.String()] = log.ReserveAddresses.SrcReserveAddress.Hex()
 		} else {
 			logger.Warnw("unexpected reserve address", "got", log.ReserveAddresses.SrcReserveAddress.Hex(), "want", "1 valid address (not default one)")

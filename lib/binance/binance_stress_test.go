34c74582a0b7a4e54e34b1360c3658a93f6267e9
Diep Pham <me@favadi.com>
2019-03-07 11:56:40 +0700

    add binance test

---
+++ lib/binance/binance_stress_test.go
@@ -0,0 +1,41 @@
+package binance
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
+)
+
+func TestBinance(t *testing.T) {
+	var (
+		apiKey    = os.Getenv("BINANCE_API_KEY")
+		secretKey = os.Getenv("BINANCE_SECRET_KEY")
+	)
+
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer logger.Sync()
+	sugar := logger.Sugar()
+
+	bn := NewBinance(apiKey, secretKey, sugar)
+
+	const requests = 10000
+	var g errgroup.Group
+	for i := 0; i < requests; i++ {
+		g.Go(func() error {
+			i := i
+			if _, err := bn.GetExchangeInfo(); err != nil {
+				return err
+			}
+			sugar.Infow("request completed", "order", i)
+			return nil
+		})
+	}
+
+	assert.NoError(t, g.Wait())
+}

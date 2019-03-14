4fc048b5e9880f0fe072ee29fc8965d017c1c859
kyberliem <cyberliem.civil@gmail.com>
2019-03-14 13:58:16 +0700

    added method to interfaces

--- accounting/reserve-rate/storage/interface.go
+++ accounting/reserve-rate/storage/interface.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"github.com/KyberNetwork/reserve-stats/lib/lastblockdaily"
 	"github.com/KyberNetwork/reserve-stats/reserverates/common"
+	"time"
 )
 
 //Interface defines a set of interface for reserve rate storage, which can be implemented by any DB
@@ -10,4 +11,6 @@ type Interface interface {
 	UpdateRatesRecords(lastblockdaily.BlockInfo, map[string]map[string]common.ReserveRateEntry) error
 	UpdateETHUSDPrice(blockInfo lastblockdaily.BlockInfo, ethusdRate float64) error
 	GetLastResolvedBlockInfo() (lastblockdaily.BlockInfo, error)
+	GetETHUSDRates(from, to time.Time) (AccountingReserveRates, error)
+	GetRates(from, to time.Time) (map[string]AccountingReserveRates, error)
 }

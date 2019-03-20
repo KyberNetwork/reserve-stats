c0b203a3717ad109392e393a1e5789e151676894
kyberliem <cyberliem.civil@gmail.com>
2019-03-20 10:38:47 +0700

    add main

---
+++ accounting/cmd/accounting-cex-withdrawal-api/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
+
+	http "github.com/KyberNetwork/reserve-stats/accounting/cex-withdrawal-api"
+	"github.com/KyberNetwork/reserve-stats/accounting/common"
+	huobiPostgres "github.com/KyberNetwork/reserve-stats/accounting/huobi/storage/withdrawal-history/postgres"
+	libapp "github.com/KyberNetwork/reserve-stats/lib/app"
+	"github.com/KyberNetwork/reserve-stats/lib/httputil"
+)
+
+const (
+	defaultPostGresDB = common.DefaultDB
+)
+
+func newServerCli() *cli.App {
+	app := libapp.NewApp()
+	app.Name = "cex-withdrawal-api"
+	app.Usage = "server for query accounting cex withdrawal"
+	app.Flags = append(app.Flags, httputil.NewHTTPCliFlags(httputil.ReserveRatesPort)...)
+	app.Flags = append(app.Flags, libapp.NewPostgreSQLFlags(defaultPostGresDB)...)
+	app.Action = run
+	return app
+}
+
+func run(c *cli.Context) error {
+	if err := libapp.Validate(c); err != nil {
+		return err
+	}
+
+	logger, err := libapp.NewLogger(c)
+	if err != nil {
+		return err
+	}
+	sugar := logger.Sugar()
+	defer logger.Sync()
+
+	db, err := libapp.NewDBFromContext(c)
+	if err != nil {
+		return err
+	}
+
+	huobiDb, err := huobiPostgres.NewDB(sugar, db)
+	if err != nil {
+		return err
+	}
+	defer func(err *error) {
+		*err = huobiDb.Close()
+	}(&err)
+	hostStr := httputil.NewHTTPAddressFromContext(c)
+	server, err := http.NewServer(hostStr, huobiDb, nil, sugar)
+	if err != nil {
+		return err
+	}
+	server.Run()
+	return err
+}
+
+func main() {
+	app := newServerCli()
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}

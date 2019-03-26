0b27c4a52acc789d61612d2f450b7c7b985d6e98
Hoàng Hà <halink0803@gmail.com>
2019-03-26 16:50:32 +0700

    initiate gateway

---
+++ accounting/cmd/accounting-gateway/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+	"github.com/urfave/cli"
+
+	"github.com/KyberNetwork/reserve-stats/gateway/http"
+	libapp "github.com/KyberNetwork/reserve-stats/lib/app"
+	"github.com/KyberNetwork/reserve-stats/lib/httputil"
+)
+
+const (
+	writeAccessKeyFlag = "write-access-key"
+	writeSecretKeyFlag = "write-secret-key"
+	readAccessKeyFlag  = "read-access-key"
+	readSecretKeyFlag  = "read-secret-key"
+	listedTokenURLFlag = "listed-token-url"
+)
+
+var (
+	//TODO: change to httputil.ListedTokenPort
+	defaultListedTokenURLValue = fmt.Sprintf("http://127.0.0.1:%d", 8010)
+)
+
+func main() {
+	app := libapp.NewApp()
+	app.Name = "Gateway"
+	app.Usage = "Reserve Stats API Gateway"
+	app.Version = "0.0.1"
+	app.Action = run
+
+	app.Flags = append(app.Flags,
+		cli.StringFlag{
+			Name:   writeAccessKeyFlag,
+			Usage:  "key for access POST/GET api",
+			EnvVar: "WRITE_ACCESS_KEY",
+		},
+		cli.StringFlag{
+			Name:   writeSecretKeyFlag,
+			Usage:  "seceret key for POST/GET api",
+			EnvVar: "WRITE_SECRET_KEY",
+		},
+		cli.StringFlag{
+			Name:   readAccessKeyFlag,
+			Usage:  "key for access GET api",
+			EnvVar: "READ_ACCESS_KEY",
+		},
+		cli.StringFlag{
+			Name:   readSecretKeyFlag,
+			Usage:  "seceret key for GET api",
+			EnvVar: "READ_SECRET_KEY",
+		},
+		cli.StringFlag{
+			Name:   listedTokenURLFlag,
+			Usage:  "listed token API URL",
+			Value:  defaultListedTokenURLValue,
+			EnvVar: "LISTED_TOKEN_URL",
+		},
+	)
+	app.Flags = append(app.Flags, httputil.NewHTTPCliFlags(httputil.GatewayPort)...)
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(c *cli.Context) error {
+	logger, err := libapp.NewLogger(c)
+	if err != nil {
+		return err
+	}
+	defer libapp.NewFlusher(logger)()
+
+	err = validation.Validate(c.String(listedTokenURLFlag),
+		validation.Required,
+		is.URL)
+	if err != nil {
+		return fmt.Errorf("invalid listed token API URL: %s", c.String(listedTokenURLFlag))
+	}
+
+	if err := validation.Validate(c.String(writeAccessKeyFlag), validation.Required); err != nil {
+		return fmt.Errorf("access key error: %s", err.Error())
+	}
+
+	if err := validation.Validate(c.String(writeSecretKeyFlag), validation.Required); err != nil {
+		return fmt.Errorf("secret key error: %s", err.Error())
+	}
+	auth, err := http.NewAuthenticator(c.String(readAccessKeyFlag), c.String(readSecretKeyFlag),
+		c.String(writeAccessKeyFlag), c.String(writeSecretKeyFlag),
+	)
+	if err != nil {
+		return fmt.Errorf("authentication object creation error: %s", err)
+	}
+	perm, err := http.NewPermissioner(c.String(readAccessKeyFlag), c.String(writeAccessKeyFlag))
+	if err != nil {
+		return fmt.Errorf("permission object creation error: %s", err)
+	}
+	svr, err := http.NewServer(httputil.NewHTTPAddressFromContext(c),
+		c.String(listedTokenURLFlag),
+		auth,
+		perm,
+		logger,
+	)
+	if err != nil {
+		return err
+	}
+	return svr.Start()
+}
---
+++ accounting/gateway/server.go
@@ -0,0 +1,67 @@
+package http
+
+import (
+	"net/http/httputil"
+	"net/url"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/httpsign"
+	ginzap "github.com/gin-contrib/zap"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+
+	libhttputil "github.com/KyberNetwork/reserve-stats/lib/httputil"
+)
+
+// Server is HTTP server of gateway service.
+type Server struct {
+	r    *gin.Engine
+	addr string
+}
+
+func newReverseProxyMW(target string) (gin.HandlerFunc, error) {
+	parsedURL, err := url.Parse(target)
+	if err != nil {
+		return nil, err
+	}
+	proxy := httputil.NewSingleHostReverseProxy(parsedURL)
+
+	return func(c *gin.Context) {
+		proxy.ServeHTTP(c.Writer, c.Request)
+	}, nil
+}
+
+// NewServer creates new instance of gateway HTTP server.
+func NewServer(addr, listedTokenURL string,
+	auth *httpsign.Authenticator,
+	perm gin.HandlerFunc,
+	logger *zap.Logger) (*Server, error) {
+	r := gin.Default()
+	r.Use(libhttputil.MiddlewareHandler)
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowAllOrigins = true
+	corsConfig.AddAllowHeaders("Digest", "Authorization", "Signature", "Nonce")
+	corsConfig.MaxAge = 5 * time.Minute
+	r.Use(ginzap.Ginzap(logger, time.RFC3339, true))
+	r.Use(cors.New(corsConfig))
+	r.Use(perm)
+	r.Use(auth.Authenticated())
+	if listedTokenURL != "" {
+		listedTokenProxyMW, err := newReverseProxyMW(listedTokenURL)
+		if err != nil {
+			return nil, err
+		}
+		r.GET("/reserve/tokens", listedTokenProxyMW)
+	}
+
+	return &Server{
+		addr: addr,
+		r:    r,
+	}, nil
+}
+
+// Start runs the HTTP gateway server.
+func (svr *Server) Start() error {
+	return svr.r.Run(svr.addr)
+}

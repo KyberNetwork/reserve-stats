fe9381ed1735a2be0020360b484a4d5c0dea0bee
Diep Pham <me@favadi.com>
2019-03-26 14:45:01 +0700

    implement accounting cex API

--- accounting/huobi/storage/interface.go
+++ accounting/huobi/storage/interface.go
@@ -1,10 +1,13 @@
 package storage
 
 import (
+	"time"
+
 	"github.com/KyberNetwork/reserve-stats/lib/huobi"
 )
 
 //Interface defines a set of interface for huobi storage, which can be implemented by any DB
 type Interface interface {
 	UpdateTradeHistory(trade []huobi.TradeHistory) error
+	GetTradeHistory(from, to time.Time) ([]huobi.TradeHistory, error)
 }

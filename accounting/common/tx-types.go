98faa5cc8e64e209b5572132ab262769134f22c8
kyberliem <cyberliem.civil@gmail.com>
2019-03-28 16:15:50 +0700

    custom marshall for erc20 transfer
    fix the custom marshall/ unmarshall on erc 20
    
    fix marshal/unmarshal method

--- accounting/common/tx-types.go
+++ accounting/common/tx-types.go
@@ -1,10 +1,14 @@
 package common
 
 import (
+	"encoding/json"
 	"math/big"
 	"time"
 
+	ethereum "github.com/ethereum/go-ethereum/common"
 	etherscan "github.com/nanmu42/etherscan-api"
+
+	"github.com/KyberNetwork/reserve-stats/lib/timeutil"
 )
 
 // NormalTx holds info from normal tx query.
@@ -37,16 +41,73 @@ type InternalTx struct {
 
 // ERC20Transfer holds info from ERC20 token transfer event query.
 type ERC20Transfer struct {
-	BlockNumber     int       `json:"blockNumber,string"`
-	TimeStamp       time.Time `json:"timeStamp"`
-	Hash            string    `json:"hash"`
-	From            string    `json:"from"`
-	ContractAddress string    `json:"contractAddress"`
-	To              string    `json:"to"`
-	Value           *big.Int  `json:"value"`
-	Gas             int       `json:"gas,string"`
-	GasUsed         int       `json:"gasUsed,string"`
-	GasPrice        *big.Int  `json:"gasPrice"`
+	BlockNumber     int              `json:"blockNumber,string"`
+	Timestamp       time.Time        `json:"-"`
+	Hash            ethereum.Hash    `json:"-"`
+	From            ethereum.Address `json:"-"`
+	ContractAddress ethereum.Address `json:"-"`
+	To              ethereum.Address `json:"-"`
+	Value           *big.Int         `json:"value"`
+	Gas             int              `json:"gas,string"`
+	GasUsed         int              `json:"gasUsed,string"`
+	GasPrice        *big.Int         `json:"gasPrice"`
+}
+
+//MarshalJSON return marshal form of erc20transfer
+func (et ERC20Transfer) MarshalJSON() ([]byte, error) {
+	type AliasErc20 ERC20Transfer
+	var ts *uint64
+	if !et.Timestamp.IsZero() {
+		millis := timeutil.TimeToTimestampMs(et.Timestamp)
+		ts = &millis
+	}
+
+	return json.Marshal(struct {
+		Timestamp       *uint64 `json:"timestamp"`
+		Hash            string  `json:"hash"`
+		From            string  `json:"from"`
+		ContractAddress string  `json:"contractAddress"`
+		To              string  `json:"to"`
+		AliasErc20
+	}{
+		Timestamp:       ts,
+		Hash:            et.Hash.Hex(),
+		From:            et.From.Hex(),
+		ContractAddress: et.ContractAddress.Hex(),
+		To:              et.ContractAddress.Hex(),
+		AliasErc20:      (AliasErc20)(et),
+	})
+}
+
+//UnmarshalJSON return an ERC20Transfer object from JSON form
+func (et *ERC20Transfer) UnmarshalJSON(data []byte) error {
+	type AliasErc20 ERC20Transfer
+	decoded := new(struct {
+		Timestamp       *uint64 `json:"timestamp,omitempty"`
+		Hash            string  `json:"hash"`
+		From            string  `json:"from"`
+		ContractAddress string  `json:"contractAddress"`
+		To              string  `json:"to"`
+		AliasErc20
+	})
+	if err := json.Unmarshal(data, decoded); err != nil {
+		return err
+	}
+	if decoded.Timestamp != nil {
+		et.Timestamp = timeutil.TimestampMsToTime(*decoded.Timestamp)
+	}
+
+	et.BlockNumber = decoded.BlockNumber
+
+	et.Hash = ethereum.HexToHash(decoded.Hash)
+	et.From = ethereum.HexToAddress(decoded.From)
+	et.To = ethereum.HexToAddress(decoded.To)
+	et.Value = decoded.Value
+	et.Gas = decoded.Gas
+	et.GasUsed = decoded.GasUsed
+	et.GasPrice = decoded.GasPrice
+
+	return nil
 }
 
 //EtherscanInternalTxToCommon transforms etherScan.InternalTx to accounting's InternalTx
@@ -68,11 +129,11 @@ func EtherscanInternalTxToCommon(tx etherscan.InternalTx) InternalTx {
 func EtherscanERC20TransferToCommon(tx etherscan.ERC20Transfer) ERC20Transfer {
 	return ERC20Transfer{
 		BlockNumber:     tx.BlockNumber,
-		TimeStamp:       tx.TimeStamp.Time(),
-		Hash:            tx.Hash,
-		From:            tx.From,
-		ContractAddress: tx.ContractAddress,
-		To:              tx.To,
+		Timestamp:       tx.TimeStamp.Time(),
+		Hash:            ethereum.HexToHash(tx.Hash),
+		From:            ethereum.HexToAddress(tx.From),
+		ContractAddress: ethereum.HexToAddress(tx.ContractAddress),
+		To:              ethereum.HexToAddress(tx.To),
 		Value:           tx.Value.Int(),
 		Gas:             tx.Gas,
 		GasUsed:         tx.GasUsed,

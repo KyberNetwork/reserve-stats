6bfec4c7b35eb10488765aace3da09d92b9e5d39
kyberliem <cyberliem.civil@gmail.com>
2019-03-28 17:56:43 +0700

    add postgres implementation

---
+++ accounting/wallet-erc20/storage/interface.go
@@ -0,0 +1,15 @@
+package storage
+
+import (
+	"time"
+
+	ethereum "github.com/ethereum/go-ethereum/common"
+
+	"github.com/KyberNetwork/reserve-stats/accounting/common"
+)
+
+//Interface defines required functions for wallet-erc20 storage
+type Interface interface {
+	UpdateERC20Transfers([]common.ERC20Transfer) error
+	GetERC20Transfers(WalletAddr, TokenAddr ethereum.Address, from, to time.Time)
+}

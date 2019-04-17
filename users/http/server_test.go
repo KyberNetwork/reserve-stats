3bd557362c18704c25e6ad54d3e204da52453885
Hoàng Hà <halink0803@gmail.com>
2019-04-17 14:02:21 +0700

    add benchmark for user api

--- users/http/server_test.go
+++ users/http/server_test.go
@@ -1,88 +1,46 @@
 package http
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/influxdata/influxdb/client/v2"
-	"github.com/jmoiron/sqlx"
-	_ "github.com/lib/pq" // sql driver name: "postgres"
-	"github.com/stretchr/testify/assert"
-	"go.uber.org/zap"
 
 	"github.com/KyberNetwork/reserve-stats/lib/httputil"
+	"github.com/KyberNetwork/reserve-stats/lib/testutil"
 	"github.com/KyberNetwork/reserve-stats/lib/tokenrate"
 	"github.com/KyberNetwork/reserve-stats/users/storage"
 )
 
 const (
-	postgresHost     = "127.0.0.1"
-	postgresPort     = 5432
-	postgresUser     = "reserve_stats"
-	postgresPassword = "reserve_stats"
-	postgresDatabase = "reserve_stats"
+	requestEndpoint = "/users"
 )
 
-func newTestDB(sugar *zap.SugaredLogger) (*storage.UserDB, error) {
-	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		postgresHost,
-		postgresPort,
-		postgresUser,
-		postgresPassword,
-		postgresDatabase,
-	)
-	db, err := sqlx.Connect("postgres", connStr)
-	if err != nil {
-		return nil, err
-	}
-	return storage.NewDB(sugar, db)
-}
+var (
+	userStorage   *storage.UserDB
+	err           error
+	influxClient  client.Client
+	influxStorage *storage.InfluxStorage
+	s             *Server
+)
 
-func tearDown(t *testing.T, storage *storage.UserDB, influxClient client.Client) {
-	assert.Nil(t, storage.DeleteAllTables(), "database should be deleted completely")
+func tearDown(storage *storage.UserDB, influxClient client.Client) error {
+	if err := storage.DeleteAllTables(); err != nil {
+		return err
+	}
 	_, err := influxClient.Query(client.Query{
 		Command: fmt.Sprintf("DROP DATABASE %s", "test_db"),
 	})
-	assert.Nil(t, err, "influx test db should be tear down successfully")
+	return err
 }
 
 func TestUserHTTPServer(t *testing.T) {
-	logger, err := zap.NewDevelopment()
-	assert.Nil(t, err, "logger should be initiated successfully")
-
-	sugar := logger.Sugar()
-	userStorage, err := newTestDB(sugar)
-	assert.Nil(t, err, "user database should be initiated successfully")
-
-	influxClient, err := client.NewHTTPClient(client.HTTPConfig{
-		Addr: "http://localhost:8086",
-	})
-	assert.Nil(t, err, "influx client should be created successfully")
-
-	defer tearDown(t, userStorage, influxClient)
-
-	// create test db
-	_, err = influxClient.Query(client.Query{
-		Command: "CREATE DATABASE test_db",
-	})
-	assert.Nil(t, err, "influx should create database successfully")
-
-	influxStorage, err := storage.NewInfluxStorage(
-		sugar,
-		"test_db",
-		influxClient,
-	)
-	assert.Nil(t, err, "influx storage should be created successfully")
-
-	s := NewServer(sugar, tokenrate.NewMock(), userStorage, "", influxStorage)
-	s.register()
-
 	// test case
-	const (
-		requestEndpoint = "/users"
-	)
-
 	var (
 		tests = []httputil.HTTPTestCase{
 			{
@@ -179,7 +137,7 @@ func TestUserHTTPServer(t *testing.T) {
     {
       "address": "0x2ea6200a999f4c6c982be525f8dc294f14f4cb08",
       "timestamp": 1538380682000
-    }
+    } 
   ]
 }`),
 				Assert: expectSuccess,
@@ -227,4 +185,90 @@ func TestUserHTTPServer(t *testing.T) {
 		tc := tc
 		t.Run(tc.Msg, func(t *testing.T) { httputil.RunHTTPTestCase(t, tc, s.r) })
 	}
+}
+
+// RunHTTPTestCase run http request test case
+func runHTTPTestCase(tc httputil.HTTPTestCase, handler http.Handler) {
+	req, err := http.NewRequest(tc.Method, tc.Endpoint, bytes.NewBuffer(tc.Body))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	q := req.URL.Query()
+	for k, v := range tc.Params {
+		q.Add(k, v)
+	}
+	req.URL.RawQuery = q.Encode()
+
+	resp := httptest.NewRecorder()
+	handler.ServeHTTP(resp, req)
+	if resp.Code != http.StatusOK {
+		log.Fatal(err)
+	}
+}
+
+func BenchmarkUserAPI(b *testing.B) {
+	var (
+		tc = httputil.HTTPTestCase{
+			Msg:      "update correct user addresses",
+			Endpoint: requestEndpoint,
+			Method:   http.MethodPost,
+			Body: []byte(`
+{
+  "email": "test@gmail.com",
+  "user_info": [
+    {
+      "address": "0xc9a658f87d7432ff897f31dce318f0856f66acb7",
+      "timestamp": 1538380670000
+    },
+    {
+      "address": "0x2ea6200a999f4c6c982be525f8dc294f14f4cb08",
+      "timestamp": 1538380682000
+    } 
+  ]
+}`),
+			Assert: expectSuccess,
+		}
+	)
+	for i := 0; i < b.N; i++ {
+		runHTTPTestCase(tc, s.r)
+	}
+}
+
+func TestMain(m *testing.M) {
+	sugar := testutil.MustNewDevelopmentSugaredLogger()
+	_, db := testutil.MustNewDevelopmentDB()
+	if userStorage, err = storage.NewDB(sugar, db); err != nil {
+		log.Fatal("user database should be initiated successfully")
+	}
+
+	if influxClient, err = client.NewHTTPClient(client.HTTPConfig{
+		Addr: "http://localhost:8086",
+	}); err != nil {
+		log.Fatal("influx client should be created successfully")
+	}
+
+	// create test db
+	if _, err = influxClient.Query(client.Query{
+		Command: "CREATE DATABASE test_db",
+	}); err != nil {
+		log.Fatal("influx should create database successfully")
+	}
+
+	if influxStorage, err = storage.NewInfluxStorage(
+		sugar,
+		"test_db",
+		influxClient,
+	); err != nil {
+		log.Fatal("influx storage should be created successfully")
+	}
+
+	s = NewServer(sugar, tokenrate.NewMock(), userStorage, "", influxStorage)
+	s.register()
+	ret := m.Run()
+	if err = tearDown(userStorage, influxClient); err != nil {
+		log.Fatal(err)
+	}
+	os.Exit(ret)
 }

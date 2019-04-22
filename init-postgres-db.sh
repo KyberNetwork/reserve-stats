1c12627b487c958288ac1bbec9d25d7007b54960
Hoàng Hà <halink0803@gmail.com>
2019-04-22 13:36:07 +0700

    add databases to init-postgres-db.sh

--- init-postgres-db.sh
+++ init-postgres-db.sh
@@ -6,4 +6,10 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE DATABASE "price-analytics";
     CREATE DATABASE "accounting";
     CREATE DATABASE "app-names";
+    CREATE DATABASE "cex_trades";
+    CREATE DATABASE "cex_withdrawals";
+    CREATE DATABASE "listed_tokens";
+    CREATE DATABASE "reserve_rates";
+    CREATE DATABASE "transactions";
+    CREATE DATABASE "reserve_addresses";
 EOSQL

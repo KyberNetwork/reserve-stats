970e179c67e87b52aad9b4a5303f2b0921740ac1
ducquangkstn <ducquang.kstn@gmail.com>
2019-06-20 14:36:58 +0700

    add reserve function optimize access query

--- tradelogs/storage/postgrestorage/volume_test.go
+++ tradelogs/storage/postgrestorage/volume_test.go
@@ -17,7 +17,7 @@ func TestTradeLogDB_GetAssetVolume(t *testing.T) {
 		fromTime    = 1539248043000
 		toTime      = 1539248666000
 		ethAmount   = 238.33849929550047
-		totalVolume = 1.006174642648189232
+		totalVolume = 1.056174642648189277
 		freq        = "h"
 		timeStamp   = "2018-10-11T09:00:00Z"
 		ethAddress  = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
@@ -47,5 +47,48 @@ func TestTradeLogDB_GetAssetVolume(t *testing.T) {
 	}
 
 	require.Equal(t, ethAmount, result.USDAmount)
-	//require.Equal(t, totalVolume, result.Volume)
+	require.Equal(t, totalVolume, result.Volume)
+}
+
+func TestTradeLogDB_GetReserveVolume(t *testing.T) {
+	const (
+		dbName = "test_rsv_volume"
+
+		// These params are expected to be change when export.dat changes.
+		fromTime    = 1539248043000
+		toTime      = 1539248666000
+		ethAmount   = 227.05539848662738
+		totalVolume = 1.006174642648189232
+		freq        = "h"
+		timeStamp   = "2018-10-11T09:00:00Z"
+		rsvAddrStr  = "0x63825c174ab367968EC60f061753D3bbD36A0D8F"
+		ethAddress  = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+	)
+
+	tldb, err := newTestTradeLogPostgresql(dbName)
+	require.NoError(t, err)
+
+	from := timeutil.TimestampMsToTime(fromTime)
+	to := timeutil.TimestampMsToTime(toTime)
+	defer func() {
+		require.NoError(t, tldb.tearDown(dbName))
+	}()
+	require.NoError(t, loadTestData(tldb.db, testDataFile))
+
+	volume, err := tldb.GetReserveVolume(ethereum.HexToAddress(rsvAddrStr), ethereum.HexToAddress(ethAddress), from, to, freq)
+	t.Logf("Volume result %v", volume)
+	if err != nil {
+		t.Fatal(err)
+	}
+	timeUnix, err := time.Parse(time.RFC3339, timeStamp)
+	if err != nil {
+		t.Fatal(err)
+	}
+	result, ok := volume[timeutil.TimeToTimestampMs(timeUnix)]
+	if !ok {
+		t.Fatalf("expect to find result at timestamp %s, yet there is none", timeUnix.Format(time.RFC3339))
+	}
+
+	require.Equal(t, ethAmount, result.USDAmount)
+	require.Equal(t, totalVolume, result.Volume)
 }

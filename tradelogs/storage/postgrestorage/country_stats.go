c49348d43764b2680ac9da92d91e2222a370e930
ducquangkstn <ducquang.kstn@gmail.com>
2019-06-21 17:04:41 +0700

    add country stats function

--- tradelogs/storage/postgrestorage/country_stats.go
+++ tradelogs/storage/postgrestorage/country_stats.go
@@ -1,11 +1,101 @@
 package postgrestorage
 
 import (
+	"github.com/KyberNetwork/reserve-stats/lib/timeutil"
 	"github.com/KyberNetwork/reserve-stats/tradelogs/common"
+	"github.com/KyberNetwork/reserve-stats/tradelogs/storage/postgrestorage/schema"
 	"time"
 )
 
-//TODO: implement this
 func (tldb *TradeLogDB) GetCountryStats(countryCode string, from, to time.Time, timezone int8) (map[uint64]*common.CountryStats, error) {
-	return nil, nil
+	logger := tldb.sugar.With("from", from, "to", to, "func",
+		"tradelogs/storage/postgresql/TradeLogDB.GetCountryStats")
+
+	var (
+		err           error
+		ethCondition  string
+		tradelogQuery string
+		timeField     = schema.BuildDateTruncField("day", timezone)
+	)
+
+	from = schema.RoundTime(from, "day", timezone)
+	to = schema.RoundTime(to, "day", timezone).Add(time.Hour * 24)
+
+	results := make(map[uint64]*common.CountryStats)
+	if ethCondition, err = schema.BuildEthWethExcludingCondition(); err != nil {
+		return nil, err
+	}
+
+	tradelogQuery = `SELECT ` + timeField + ` AS time, 
+		COUNT(DISTINCT(user_address_id)) AS unique_address,
+		SUM(src_burn_amount) + SUM(dst_burn_amount) AS total_burn_fee
+		FROM "` + schema.TradeLogsTableName + `"
+		WHERE timestamp >= $1 AND timestamp < $2 AND country = $3
+		GROUP BY time
+	`
+	logger.Debugw("prepare statement", "stmt", tradelogQuery)
+	var datas2 []struct {
+		Time          time.Time `db:"time"`
+		UniqueAddress uint64    `db:"unique_address"`
+		TotalBurnFee  float64   `db:"total_burn_fee"`
+	}
+	if err = tldb.db.Select(&datas2, tradelogQuery, from.UTC().Format(schema.DefaultDateFormat),
+		to.UTC().Format(schema.DefaultDateFormat), countryCode); err != nil {
+		return nil, err
+	}
+
+	if len(datas2) == 0 {
+		return nil, nil
+	}
+
+	for _, data := range datas2 {
+		ts := timeutil.TimeToTimestampMs(data.Time)
+		results[ts] = &common.CountryStats{
+			UniqueAddresses: data.UniqueAddress,
+			TotalBurnFee:    data.TotalBurnFee,
+		}
+	}
+
+	tradelogQuery =
+		`SELECT ` + timeField + ` AS time, 
+		SUM(eth_amount) total_eth_volume, 
+		AVG(eth_amount) eth_per_trade,
+		SUM(eth_amount*eth_usd_rate) as total_usd_volume, 
+		AVG(eth_amount*eth_usd_rate) usd_per_trade, count(1) as total_trade 
+	FROM ` + schema.TradeLogsTableName + `
+	WHERE ` + ethCondition + `
+	AND timestamp >= $1 AND timestamp < $2 AND country = $3
+	GROUP BY time
+	`
+	logger.Debugw("prepare statement", "stmt", tradelogQuery)
+	var datas []struct {
+		Time           time.Time `db:"time"`
+		TotalEthVolume float64   `db:"total_eth_volume"`
+		EthPerTrade    float64   `db:"eth_per_trade"`
+		TotalUsdVolume float64   `db:"total_usd_volume"`
+		UsdPerTrade    float64   `db:"usd_per_trade"`
+		TotalTrade     uint64    `db:"total_trade"`
+	}
+
+	err = tldb.db.Select(&datas, tradelogQuery, from.UTC().Format(schema.DefaultDateFormat),
+		to.UTC().Format(schema.DefaultDateFormat), countryCode)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, data := range datas {
+		ts := timeutil.TimeToTimestampMs(data.Time)
+		summary, ok := results[ts]
+		if !ok {
+			logger.Warn("key not found", "ts", ts)
+			continue
+		}
+		summary.TotalUSDVolume = data.TotalUsdVolume
+		summary.TotalETHVolume = data.TotalEthVolume
+		summary.ETHPerTrade = data.EthPerTrade
+		summary.USDPerTrade = data.UsdPerTrade
+		summary.TotalTrade = data.TotalTrade
+	}
+
+	return results, nil
 }
---
+++ tradelogs/storage/postgrestorage/country_stats_test.go
@@ -0,0 +1,49 @@
+package postgrestorage
+
+import (
+	"github.com/KyberNetwork/reserve-stats/lib/timeutil"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestTradeLogDB_GetCountryStats(t *testing.T) {
+	const (
+		dbName  = "test_country_stats_volume"
+		country = "DE"
+
+		ethExpectedValue     = 0.47151091316103044
+		usdExpectedValue     = 106.40210331359417
+		burnFeeExpectedValue = 0.6542213920109298
+
+		timeStamp = "2018-10-11T08:00:00Z"
+	)
+
+	var (
+		fromTime = timeutil.TimestampMsToTime(1539216000000)
+		toTime   = timeutil.TimestampMsToTime(1539254666000)
+		timeZone = int8(-8)
+	)
+
+	timeUnix, err := time.Parse(time.RFC3339, timeStamp)
+	require.NoError(t, err)
+	timeUint := timeutil.TimeToTimestampMs(timeUnix)
+
+	tldb, err := newTestTradeLogPostgresql(dbName)
+	require.NoError(t, err)
+
+	defer func() {
+		require.NoError(t, tldb.tearDown(dbName))
+	}()
+
+	require.NoError(t, loadTestData(tldb.db, testDataFile))
+	integrationVol, err := tldb.GetCountryStats(country,
+		fromTime, toTime, timeZone)
+	require.NoError(t, err)
+	require.Contains(t, integrationVol, timeUint)
+	t.Logf("%+v", integrationVol[timeUint])
+
+	require.Equal(t, ethExpectedValue, integrationVol[timeUint].TotalETHVolume)
+	require.Equal(t, usdExpectedValue, integrationVol[timeUint].TotalUSDVolume)
+	require.Equal(t, burnFeeExpectedValue, integrationVol[timeUint].TotalBurnFee)
+}

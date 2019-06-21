19eb2ced09c32c080cb898405064a338bca43de6
ducquangkstn <ducquang.kstn@gmail.com>
2019-06-21 13:28:59 +0700

    add integration function

--- tradelogs/storage/postgrestorage/integration_volume.go
+++ tradelogs/storage/postgrestorage/integration_volume.go
@@ -1,11 +1,52 @@
 package postgrestorage
 
 import (
+	appname "github.com/KyberNetwork/reserve-stats/app-names"
+	"github.com/KyberNetwork/reserve-stats/lib/timeutil"
 	"github.com/KyberNetwork/reserve-stats/tradelogs/common"
+	"github.com/KyberNetwork/reserve-stats/tradelogs/storage/postgrestorage/schema"
 	"time"
 )
 
-//TODO: implement this
 func (tldb *TradeLogDB) GetIntegrationVolume(fromTime, toTime time.Time) (map[uint64]*common.IntegrationVolume, error) {
-	return nil, nil
+	logger := tldb.sugar.With("from", fromTime, "to", toTime, "func",
+		"tradelogs/storage/postgresql/TradeLogDB.GetIntegrationVolume")
+
+	integrationQuery :=
+		`SELECT
+			SUM(eth_amount * (CASE WHEN integration_app = '` + appname.KyberSwapAppName + `' then 1 else 0 end)) as integration_volume,
+			SUM(eth_amount * (CASE WHEN integration_app != '` + appname.KyberSwapAppName + `' then 1 else 0 end)) as non_integration_volume,
+			` + schema.BuildDateTruncField("day", 0) + ` AS time
+		FROM ` + schema.TradeLogsTableName + ` WHERE timestamp >= $1 and timestamp < $2
+		GROUP BY time
+		`
+	fromTime = schema.RoundTime(fromTime, "day", 0)
+	toTime = schema.RoundTime(toTime, "day", 0).Add(time.Hour * 24)
+
+	var datas []struct {
+		Timestamp            time.Time `db:"time"`
+		IntegrationVolume    float64   `db:"integration_volume"`
+		NonIntegrationVolume float64   `db:"non_integration_volume"`
+	}
+
+	logger.Debugw("prepare statement", "stmt", integrationQuery)
+	err := tldb.db.Select(&datas, integrationQuery, fromTime.UTC().Format(schema.DefaultDateFormat),
+		toTime.UTC().Format(schema.DefaultDateFormat))
+	if err != nil {
+		return nil, err
+	}
+
+	if len(datas) == 0 {
+		logger.Debugw("no trade found")
+		return nil, nil
+	}
+	results := make(map[uint64]*common.IntegrationVolume)
+	for _, data := range datas {
+		ts := timeutil.TimeToTimestampMs(data.Timestamp)
+		results[ts] = &common.IntegrationVolume{
+			KyberSwapVolume:    data.IntegrationVolume,
+			NonKyberSwapVolume: data.NonIntegrationVolume,
+		}
+	}
+	return results, nil
 }
---
+++ tradelogs/storage/postgrestorage/integration_volume_test.go
@@ -0,0 +1,49 @@
+package postgrestorage
+
+import (
+	"github.com/KyberNetwork/reserve-stats/lib/timeutil"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestTradeLogDB_GetIntegrationVolume(t *testing.T) {
+	const (
+		dbName = "test_integration_volume"
+		// These params are expected to be change when export.dat changes.
+
+		timeStamp = "2018-10-11T00:00:00Z"
+	)
+
+	var (
+		fromTime = timeutil.TimestampMsToTime(1539216000000)
+		toTime   = timeutil.TimestampMsToTime(1539254666000)
+	)
+
+	tldb, err := newTestTradeLogPostgresql(dbName)
+	require.NoError(t, err)
+	defer func() {
+		require.NoError(t, tldb.tearDown(dbName))
+	}()
+
+	require.NoError(t, loadTestData(tldb.db, testDataFile))
+
+	integrationVol, err := tldb.GetIntegrationVolume(fromTime, toTime)
+
+	require.NoError(t, err)
+
+	timeUnix, err := time.Parse(time.RFC3339, timeStamp)
+	assert.NoError(t, err)
+	timeUint := timeutil.TimeToTimestampMs(timeUnix)
+	result, ok := integrationVol[timeUint]
+	if !ok {
+		t.Fatalf("expect to find result at timestamp %s, yet there is none", timeUnix.Format(time.RFC3339))
+	}
+	assert.NotZero(t, result.KyberSwapVolume)
+	assert.NotZero(t, result.NonKyberSwapVolume)
+
+	assert.Equal(t, float64(5.3909054905423455), result.KyberSwapVolume)
+	assert.Equal(t, float64(12), result.NonKyberSwapVolume)
+
+}

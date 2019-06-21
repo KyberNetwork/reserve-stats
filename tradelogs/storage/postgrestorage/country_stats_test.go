442cefde28e9e2f4f271e51607799006fa0a0659
ducquangkstn <ducquang.kstn@gmail.com>
2019-06-21 17:04:41 +0700

    add country stats function

--- tradelogs/storage/postgrestorage/country_stats_test.go
+++ tradelogs/storage/postgrestorage/country_stats_test.go
@@ -1,31 +1,13 @@
-package influxstorage
+package postgrestorage
 
 import (
+	"github.com/KyberNetwork/reserve-stats/lib/timeutil"
+	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
-
-	"github.com/stretchr/testify/require"
-
-	"github.com/KyberNetwork/reserve-stats/lib/timeutil"
-	tradelogcq "github.com/KyberNetwork/reserve-stats/tradelogs/storage/influxstorage/cq"
 )
 
-func aggregateCountryStats(is *InfluxStorage) error {
-	cqs, err := tradelogcq.CreateCountryCqs(is.dbName)
-	if err != nil {
-		return err
-	}
-	for _, cq := range cqs {
-		err = cq.Execute(is.influxClient, is.sugar)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-
-func TestInfluxStorage_GetCountryStats(t *testing.T) {
+func TestTradeLogDB_GetCountryStats(t *testing.T) {
 	const (
 		dbName  = "test_country_stats_volume"
 		country = "DE"
@@ -47,16 +29,16 @@ func TestInfluxStorage_GetCountryStats(t *testing.T) {
 	require.NoError(t, err)
 	timeUint := timeutil.TimeToTimestampMs(timeUnix)
 
-	is, err := newTestInfluxStorage(dbName)
+	tldb, err := newTestTradeLogPostgresql(dbName)
 	require.NoError(t, err)
 
 	defer func() {
-		require.NoError(t, is.tearDown())
+		require.NoError(t, tldb.tearDown(dbName))
 	}()
 
-	require.NoError(t, loadTestData(dbName))
-	require.NoError(t, aggregateCountryStats(is))
-	integrationVol, err := is.GetCountryStats(country, fromTime, toTime, timeZone)
+	require.NoError(t, loadTestData(tldb.db, testDataFile))
+	integrationVol, err := tldb.GetCountryStats(country,
+		fromTime, toTime, timeZone)
 	require.NoError(t, err)
 	require.Contains(t, integrationVol, timeUint)
 	t.Logf("%+v", integrationVol[timeUint])

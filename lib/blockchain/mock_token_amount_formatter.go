0923290c3a88c7c34c98c5ae1004d5ea503ab3c7
ducquangkstn <ducquang.kstn@gmail.com>
2019-10-21 15:34:04 +0700

    allow overwrite when re-run tradelog with psql db

--- lib/blockchain/mock_token_amount_formatter.go
+++ lib/blockchain/mock_token_amount_formatter.go
@@ -16,12 +16,25 @@ type MockTokenAmountFormatter struct {
 }
 
 // ToWei return the given human friendly number to wei unit.
-func (mc *MockTokenAmountFormatter) ToWei(_ common.Address, _ float64) (*big.Int, error) {
-	return big.NewInt(100), nil
+func (mc *MockTokenAmountFormatter) ToWei(_ common.Address, amount float64) (*big.Int, error) {
+	decimals := 18
+	exp := big.NewInt(0).Exp(big.NewInt(10), big.NewInt(int64(decimals)), nil)
+	result, _ := big.NewFloat(0).Mul(big.NewFloat(amount), big.NewFloat(0).SetInt(exp)).Int(nil)
+	return result, nil
 }
 
 // FromWei formats the given amount in wei to human friendly
 // number with preconfigured token decimals.
-func (mc *MockTokenAmountFormatter) FromWei(_ common.Address, _ *big.Int) (float64, error) {
-	return 9.99, nil
+func (mc *MockTokenAmountFormatter) FromWei(_ common.Address, amount *big.Int) (float64, error) {
+	if amount == nil {
+		return 0, nil
+	}
+	decimals := 18
+	floatAmount := new(big.Float).SetInt(amount)
+	power := new(big.Float).SetInt(new(big.Int).Exp(
+		big.NewInt(10), big.NewInt(int64(decimals)), nil,
+	))
+	res := new(big.Float).Quo(floatAmount, power)
+	result, _ := res.Float64()
+	return result, nil
 }

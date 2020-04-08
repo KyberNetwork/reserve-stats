6d18321fe1bcc72aff7a18cdba6d588227d5bbe0
Hoàng Hà <halink0803@gmail.com>
2020-04-08 15:30:42 +0700

    update data file for test

--- tradelogs/storage/testdata/postgresql/export.sh
+++ tradelogs/storage/testdata/postgresql/export.sh
@@ -6,24 +6,24 @@ set -euo pipefail
 
 readonly from_block=6494037
 readonly to_block=6494137
-readonly postgresql_image='postgres:9.5.14-alpine'
+readonly postgresql_image='postgres:10-alpine'
 readonly container_name='trade-logs-sample-data-exporter'
 readonly data_dir='/tmp/trade-logs-sample-data-exporter'
 
 
 docker run --rm --detach --name "$container_name" \
     --volume "$data_dir:/var/lib/postgresql/data" \
-    --publish '127.0.0.1:5433:5432' \
+    --publish '127.0.0.1:5432:5432' \
     -e "POSTGRES_PASSWORD=reserve_stats" -e "POSTGRES_USER=reserve_stats" -e "POSTGRES_DB=reserve_stats" \
     "$postgresql_image"
 
-sleep 30
+sleep 60
 
 pushd ../../../cmd/trade-logs-crawler
 echo "start build"
 go build
 echo "start"
-./trade-logs-crawler --db-engine=postgres --postgres-host=127.0.0.1 --postgres-port=5433 \
+./trade-logs-crawler --db-engine=postgres --postgres-host=127.0.0.1 --postgres-port=5432 \
     --from-block "$from_block" --to-block "$to_block" $TL_CRAWLER_PARAMS
 echo "done"
 popd

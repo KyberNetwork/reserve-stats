10d09ad99c01b63cc93b1952374310cb9378f1d7
halink0803 <halink0803@gmail.com>
2021-04-13 14:41:17 +0700

    update prod contracts

--- lib/contracts/addresses.go
+++ lib/contracts/addresses.go
@@ -37,12 +37,12 @@ var (
 		[]common.Address{},
 	)
 	internalReserveAddress = deployment.NewAddress(
-		[]common.Address{common.HexToAddress("")},
+		[]common.Address{common.HexToAddress("0x214d6546f862c2d36ab6cc7f3ddb3423579dfad3")},
 		[]common.Address{common.HexToAddress("0xb3F787E2A95326C9dBb0e43151e9B94B7f850e7b")},
 		[]common.Address{common.HexToAddress("0xc1F6B9c8c79fD6BE87aB017566A066c10e92e7e0")},
 	)
 	pricingContractAddress = deployment.NewAddress(
-		[]common.Address{},
+		[]common.Address{common.HexToAddress("0x58F9546fdE73Ca379fB388BA83292dD9D366156F")},
 		[]common.Address{common.HexToAddress("0xD14b35Af7316260454B83739aA377667fDA22627")},
 		[]common.Address{common.HexToAddress("0xfe96C7566c5f5D5555EbDC88B687456081114EBe")},
 	)

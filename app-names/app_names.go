b257f1f07b5c18da669a545195cbdbd8af145398
halink0803 <halink0803@gmail.com>
2021-12-27 10:12:28 +0000

    remove unused services from ci

--- app-names/app_names.go
+++
@@ -1,8 +0,0 @@
-package appname
-
-const (
-	// KyberSwapAppName is the name of kyberswap
-	KyberSwapAppName = "KyberSwap"
-	// ThirdPartyAppName is the generic name for third party apps
-	ThirdPartyAppName = "ThirdParty"
-)
